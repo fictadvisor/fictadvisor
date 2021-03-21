d3250904ed99f02b7bc7bdfe73fa19d1159047f3
Alexander Nazarov <alxkrekeris@gmail.com>
2021-03-21 18:02:34 +0200

    added viewport

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -17,6 +17,7 @@ export default function PageLayout({ meta, title, children }: PageLayoutProperti
     <div>
         <Head>
           <title>{metaTitle}</title>
+          <meta name="viewport" content="width=700, initial-scale=1"></meta>
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
