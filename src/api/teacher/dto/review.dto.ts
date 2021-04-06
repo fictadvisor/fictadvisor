16cf430ea1d70d2b4768815a2e67c9cd68dd4c0f
MaryLynJuana <mary_juana@ukr.net>
2021-04-06 12:46:47 +0300

    refactor(api): Move course attributes to a separate class

--- src/api/teacher/dto/review.dto.ts
+++ src/api/teacher/dto/review.dto.ts
@@ -2,15 +2,29 @@ import { Expose } from "class-transformer";
 import { assign } from "src/common/common.object";
 import { ReviewView } from "src/database/entities/review-view.entity";
 
+class Course {
+	id: string;
+
+	name: string;
+
+	link: string;
+
+	constructor(id: string, name: string, link: string) {
+		this.id = id;
+		this.name = name;
+		this.link = link;
+	}
+}
+
 export class ReviewDto {
 	id: string;
+
 	content: string;
-	course: {
-		id: string;
-		name: string;
-		link: string;
-	};
+
+	course: Course;
+
 	rating: number;
+
 	date: Date;
 
 	public static from(e: ReviewView) {
@@ -19,13 +33,14 @@ export class ReviewDto {
             {
             	id: e.id,
             	content: e.content,
-            	course: {
-            		id: e.courseId,
-            		name: e.courseName,
-            		link: e.courseLink
-            	},
+            	course: new Course(
+            		e.courseId,
+            		e.courseName,
+            		e.courseLink
+            	),
             	rating: e.rating,
             	date: e.date
             }
+        );
 	}
 }
