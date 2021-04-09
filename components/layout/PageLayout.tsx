b50eb91e9c8076626b612129637b9071632b8b69
Alexander Nazarov <alxkrekeris@gmail.com>
2021-04-09 23:32:47 +0300

    fixed viewport width

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -17,7 +17,7 @@ const PageLayout = ({ meta, title, children }: PageLayoutProperties) => {
     <div className="page">
         <Head>
           <title>{metaTitle}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          <meta name="viewport" content="width=840, initial-scale=1"></meta>
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
