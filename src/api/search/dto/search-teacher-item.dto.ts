b239a13e9e0b328d9257a83fd65a14d8049eebae
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2021-04-18 16:22:36 +0300

    #19: /search endpoint implemented

---
+++ src/api/search/dto/search-teacher-item.dto.ts
@@ -0,0 +1,34 @@
+import { Expose } from 'class-transformer';
+import { TeacherSearchIndex } from '../../../database/entities/teacher-search-index.entity';
+import { assign } from '../../../common/common.object';
+
+export class SearchTeacherItemDto {
+    id: string;
+
+    link: string;
+
+    @Expose({ name: 'first_name' })
+    firstName: string;
+
+    @Expose({ name: 'middle_name' })
+    middleName?: string;
+
+    @Expose({ name: 'last_name' })
+    lastName: string;
+
+    rating: number;
+
+    public static from(i: TeacherSearchIndex): SearchTeacherItemDto {
+        return assign(
+            new SearchTeacherItemDto(),
+            {
+                id: i.id,
+                link: i.link,
+                firstName: i.firstName,
+                middleName: i.middleName,
+                lastName: i.lastName,
+                rating: i.rating,
+            }
+        )
+    }
+}
---
+++ src/api/search/search.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { SearchService } from './search.service';
+import { SearchableQueryDto } from '../../common/common.dto';
+import { SearchResultDto } from './dto/search-result.dto';
+
+@Controller('search')
+export class SearchController {
+    constructor(
+        private searchService: SearchService
+    ) {}
+
+    @Get()
+    getSearchResult(@Query() query: SearchableQueryDto): Promise<SearchResultDto> {
+        return this.searchService.searchResult(query);
+    }
+}
