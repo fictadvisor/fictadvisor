e39bdf3d535a4e9429e7a158fd27b35f7be3f149
Alexander Nazarov <alxkrekeris@gmail.com>
2021-05-17 20:31:52 +0300

    added error pages

---
+++ pages/404.tsx
@@ -0,0 +1,30 @@
+import api from "../lib/api";
+
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { GetServerSideProps } from "next";
+
+import PageLayout from "../components/layout/PageLayout";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <PageLayout meta={{ title: 'Сторінку не знайдено' }}>
+      <div className="page-error">
+        <div className="page-error-description">
+          <p className="page-error-title">Сторінку не знайдено</p>
+          <p>
+            Ми не змогли знайти потрібну тобі сторінку.
+            <br />
+            Пропонуємо повернутись на <Link href="/"><a>головну сторінку</a></Link>.
+          </p>
+        </div>
+        <div style={{ margin: '30px auto 0 auto', opacity: '0.5', textAlign: 'center' }}>
+          <img width="400px" src="/assets/404.gif" />
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default NotFoundPage;
