f4713e06958ea29f9237e9cfba152b7cf4b40504
just-hoshinon <mwadman2018@gmail.com>
2022-12-25 02:23:19 +0200

    adding jwt verifying

---
+++ src/v2/api/auth/AuthService.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../database/PrismaService';
+import { JwtPayload } from '../../security/JwtPayload';
+import { SecurityConfigService } from '../../config/SecurityConfigService';
+import { User } from '@prisma/client'
+import { TokensDTO } from './dto/TokensDTO';
+import { RegistrationDTO } from './dto/RegistrationDTO';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private securityConfig: SecurityConfigService
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    return await this.prisma.user.findFirst({
+      where: {
+        OR: [
+          { username: username },
+          { email: username }
+        ],
+        password: password
+      }
+    });
+  }
+
+  async register(user: RegistrationDTO): Promise<TokensDTO> {
+    const { email, password, username, isCaptain, ...student} = user;
+    const dbUser = await this.prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+        student: {
+          create: student
+        }
+      }
+    })
+
+    if (isCaptain) this.verifyCaptain();
+    else this.verifyStudent();
+
+    return this.getTokens(dbUser);
+  }
+
+  verifyStudent() {
+    return;
+  }
+
+  verifyCaptain() {
+    return;
+  }
+
+  login(user: User): TokensDTO {
+    return this.getTokens(user);
+  }
+
+  async refresh(refreshToken: string): Promise<object | null> {
+    try {
+      const payload: JwtPayload = await this.jwtService.verify(refreshToken);
+      return this.getAccessToken(payload);
+    } catch (err) {
+      return null;
+    }
+  }
+
+  getTokens(user: User): TokensDTO {
+    const payload: JwtPayload = {
+      sub: user.id,
+      username: user.username
+    }
+
+    return {
+      refreshToken: this.jwtService.sign(payload, {
+        expiresIn: this.securityConfig.jwtRefreshTtl,
+      }),
+      accessToken: this.jwtService.sign(payload)
+    }
+  }
+
+  getAccessToken(payload: JwtPayload) {
+    return {
+      accessToken: this.jwtService.sign(payload)
+    }
+  }
+}
