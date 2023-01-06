24e54519c39f22df872a5147cbdfc8968c20bd34
just-hoshinon <mwadman2018@gmail.com>
2023-01-06 02:35:26 +0200

    updating ScheduleModule.ts

--- src/v2/api/schedule/ScheduleModule.ts
+++ src/v2/api/schedule/ScheduleModule.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { ScheduleController } from './ScheduleController';
 import { ScheduleService } from './ScheduleService';
 import { PrismaService } from '../../database/PrismaService';
+import { ParserModule } from '../../utils/parser/ParserModule';
+import { GroupModule } from '../group/GroupModule';
+import { DateModule } from '../../utils/date/DateModule';
+import { SubjectModule } from '../subject/SubjectModule';
+import { DisciplineModule } from '../discipline/DisciplineModule';
+import { JwtGuard } from '../../security/JwtGuard';
+import { TeacherModule } from '../teacher/TeacherModule';
+import { ScheduleRepository } from './ScheduleRepository';
+import { GroupBySemesterLessonGuard } from '../../security/group-guard/GroupBySemesterLessonGuard';
+import { GroupByTemporaryLessonGuard } from '../../security/group-guard/GroupByTemporaryLessonGuard';
 
 @Module({
   controllers: [ScheduleController],
-  providers: [ScheduleService, PrismaService],
+  providers: [
+    ScheduleService,
+    PrismaService,
+    JwtGuard,
+    GroupBySemesterLessonGuard,
+    GroupByTemporaryLessonGuard,
+    ScheduleRepository,
+  ],
   exports: [ScheduleService],
+  imports: [ParserModule, GroupModule, DateModule, SubjectModule, DisciplineModule, TeacherModule]
 })
 export class ScheduleModule {}
