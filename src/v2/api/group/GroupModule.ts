7f693a69298dd13f241a7c16046db5dc788c0d63
just-hoshinon <mwadman2018@gmail.com>
2023-01-06 02:31:25 +0200

    updating group module

--- src/v2/api/group/GroupModule.ts
+++ src/v2/api/group/GroupModule.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../database/PrismaService';
 import { GroupController } from './GroupController';
 import { GroupService } from './GroupService';
+import { GroupByIdPipe } from './GroupByIdPipe';
 
 @Module({
   controllers: [GroupController],
-  providers: [PrismaService, GroupService],
-  exports: [GroupService],
+  providers: [PrismaService, GroupService, GroupByIdPipe],
+  exports: [GroupService, GroupByIdPipe],
 })
 export class GroupModule {}
