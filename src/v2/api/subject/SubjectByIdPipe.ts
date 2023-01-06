966ce0bab1a4a37174c7a771f6d82db09aafbafe
just-hoshinon <mwadman2018@gmail.com>
2023-01-06 02:34:52 +0200

    adding SubjectByIdPipe.ts

---
+++ src/v2/api/subject/SubjectByIdPipe.ts
@@ -0,0 +1,19 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { Subject } from '@prisma/client';
+import { SubjectService } from './SubjectService';
+import { InvalidSubjectIdException } from '../../utils/exceptions/InvalidSubjectIdException';
+
+@Injectable()
+export class SubjectByIdPipe implements PipeTransform<string, Promise<Subject>> {
+  constructor(
+    private subjectService: SubjectService
+  ) {}
+
+  async transform(value: string, metadata: ArgumentMetadata): Promise<Subject> {
+    const subject: Subject = await this.subjectService.get(value);
+    if (!subject) {
+      throw new InvalidSubjectIdException();
+    }
+    return subject;
+  }
+}
