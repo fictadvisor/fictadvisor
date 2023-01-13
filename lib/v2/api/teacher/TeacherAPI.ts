351055cb9bd3118cb7641c1b2972a0af5a17dbdf
kujo205 <kuc8301@gmail.com>
2023-01-13 15:11:49 +0200

    added teacher dtos and teacher api endpoints

--- lib/v2/api/teacher/TeacherAPI.ts
+++ lib/v2/api/teacher/TeacherAPI.ts
@@ -1,1 +1,24 @@
-export class TeacherAPI {}
+import { client, getAuthorizationHeader, QueryParams } from "../index";
+import { GetTeacherDTO } from "./dto/GetTeacherDTO";
+import { CreateTeacherBodyDTO } from "./dto/CreateTeacherBodyDTO";
+import { RequestObject } from "./dto/RequestObject";
+
+export class TeacherAPI {
+    static async get( accessToken:string,teacherId:string): Promise<GetTeacherDTO> {
+        return (await (client.get(`/teachers/${teacherId}`
+        ,getAuthorizationHeader(accessToken)))).data;
+    }
+
+    static async getAll(accessToken: string,RequestObject): Promise<GetTeacherDTO[]> {
+        return (await (client.get(`/teachers?search=${RequestObject?.search}`
+        ,getAuthorizationHeader(accessToken)))).data;
+    }
+
+    static async create(accessToken: string, body: CreateTeacherBodyDTO) {
+        await client.post('/teachers',
+            body,
+            getAuthorizationHeader(accessToken)
+        );
+    }
+
+}
