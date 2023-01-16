03dfb5c4590ce1e26aa5766b3b0b91da19893b7a
mibal_ua <9mohapx9@gmail.com>
2023-01-16 09:44:17 +0100

    Implement `getSchedule` method

--- lib/v2/api/schedule/ScheduleAPI.ts
+++ lib/v2/api/schedule/ScheduleAPI.ts
@@ -1,4 +1,5 @@
 import { GetSessionScheduleDTO } from "./dto/GetSessionScheduleDTO";
+import { GetScheduleDTO } from "./dto/GetScheduleDTO";
 import { client, getAuthorizationHeader } from "../index";
 
 export class ScheduleAPI {
@@ -7,5 +8,12 @@ export class ScheduleAPI {
         return (await client.get(`/group/${groupId}/session`,
             getAuthorizationHeader(accessToken))).data;
     }
+
+    static async getSchedule(accessToken: string, id: string, fortnight: string): Promise<GetScheduleDTO>{
+        return (await client.get(`/groups/${id}/static/?${fortnight}`,
+            getAuthorizationHeader(accessToken))).data;
+    }
+
+
 
 }
