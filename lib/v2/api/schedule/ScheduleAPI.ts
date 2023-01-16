0bbe926c76c95ddbd31d10e4658e9b88f23982e7
mibal_ua <9mohapx9@gmail.com>
2023-01-16 10:45:11 +0100

    Fix readability of methods

--- lib/v2/api/schedule/ScheduleAPI.ts
+++ lib/v2/api/schedule/ScheduleAPI.ts
@@ -6,21 +6,23 @@ import { client, getAuthorizationHeader } from "../index";
 
 export class ScheduleAPI {
 
-    static async getSession(accessToken: string, groupId: string): Promise<GetSessionScheduleDTO> {
+    static async getSession(accessToken: string,
+                            groupId: string): Promise<GetSessionScheduleDTO> {
         return (await client.get(`/group/${groupId}/session`,
             getAuthorizationHeader(accessToken))).data;
     }
 
-    static async getSchedule(accessToken: string, id: string, fortnight: string): Promise<GetScheduleDTO> {
+    static async getSchedule(accessToken: string,
+                             id: string,
+                             fortnight: string): Promise<GetScheduleDTO> {
         return (await client.get(`/groups/${id}/static/?${fortnight}`,
             getAuthorizationHeader(accessToken))).data;
     }
 
-    static async getDynamicLessons(accessToken: string, id: string, fortnight: string): Promise<GetDynamicLessonsDTO>{
+    static async getDynamicLessons(accessToken: string,
+                                   id: string,
+                                   fortnight: string): Promise<GetDynamicLessonsDTO>{
         return (await client.get(`schedule/groups/${id}/temporary/?${fortnight}`,
             getAuthorizationHeader(accessToken))).data;
     }
-
-
-
 }
