556b66b44d7d97559429eedcd0fc65cbe30abf5c
mibal_ua <9mohapx9@gmail.com>
2023-01-16 10:35:13 +0100

    Implement `getDynamicLessons` method

--- lib/v2/api/schedule/ScheduleAPI.ts
+++ lib/v2/api/schedule/ScheduleAPI.ts
@@ -1,5 +1,7 @@
 import { GetSessionScheduleDTO } from "./dto/GetSessionScheduleDTO";
 import { GetScheduleDTO } from "./dto/GetScheduleDTO";
+import { GetDynamicLessonsDTO } from "./dto/GetDynamicLessonsDTO";
+
 import { client, getAuthorizationHeader } from "../index";
 
 export class ScheduleAPI {
@@ -9,8 +11,13 @@ export class ScheduleAPI {
             getAuthorizationHeader(accessToken))).data;
     }
 
-    static async getSchedule(accessToken: string, id: string, fortnight: string): Promise<GetScheduleDTO>{
+    static async getSchedule(accessToken: string, id: string, fortnight: string): Promise<GetScheduleDTO> {
         return (await client.get(`/groups/${id}/static/?${fortnight}`,
+            getAuthorizationHeader(accessToken))).data;
+    }
+
+    static async getDynamicLessons(accessToken: string, id: string, fortnight: string): Promise<GetDynamicLessonsDTO>{
+        return (await client.get(`schedule/groups/${id}/temporary/?${fortnight}`,
             getAuthorizationHeader(accessToken))).data;
     }
 
