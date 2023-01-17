98884a540abdeb7d9721462f53be605792da7b32
Victoria Hlushkova <gl9victoria@gmail.com>
2023-01-17 20:11:55 +0200

    Create two endpoints

--- lib/v2/api/poll/PollAPI.ts
+++ lib/v2/api/poll/PollAPI.ts
@@ -1,1 +1,18 @@
-export class PollAPI {}
+import { client, getAuthorizationHeader } from "../index";
+import { CreateTeacherGradeBody } from "./dto/CreateTeacherGradeBody";
+import { GetTeacherQuestionsDTO } from "./dto/GetTeacherQuestionsDTO";
+
+
+export class PollAPI {
+    static async getTeacherQuestions(accessToken: string, disciplineTeacherId: string): Promise<GetTeacherQuestionsDTO> {
+        return (await client.get(`/poll/answers/${disciplineTeacherId}`, getAuthorizationHeader(accessToken))).data;
+    }
+    
+	static async createTeacherGrade(accessToken: string, body: CreateTeacherGradeBody, disciplineTeacherId: string) {
+        return await client.post(
+			`/poll/answers/${disciplineTeacherId}`, 
+			body, 
+			getAuthorizationHeader(accessToken)
+			);
+    }
+}
---
+++ lib/v2/api/poll/dto/CreateTeacherGradeBody.ts
@@ -0,0 +1,6 @@
+export type CreateTeacherGradeBody = {
+	answers: [{ 
+	questionId: string, 
+	value: string, 
+	}]
+}
