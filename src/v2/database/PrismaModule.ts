bfa6be048709bd3bc82292dbcc4f8fbd4c1e3593
just-hoshinon <mwadman2018@gmail.com>
2023-01-18 20:50:53 +0200

    adding new repositories to db module

--- src/v2/database/PrismaModule.ts
+++ src/v2/database/PrismaModule.ts
@@ -5,9 +5,32 @@ import { DisciplineRepository } from '../api/discipline/DisciplineRepository';
 import { DisciplineTeacherRepository } from '../api/teacher/DisciplineTeacherRepository';
 import { TeacherRepository } from '../api/teacher/TeacherRepository';
 import { GroupRepository } from '../api/group/GroupRepository';
+import { DisciplineTeacherRoleRepository } from '../api/teacher/DisciplineTeacherRoleRepository';
+import { SubjectRepository } from '../api/subject/SubjectRepository';
+import { ScheduleRepository } from '../api/schedule/ScheduleRepository';
 
 @Module({
-  providers: [PrismaService, DisciplineTypeRepository, DisciplineRepository, DisciplineTeacherRepository, TeacherRepository, GroupRepository],
-  exports: [PrismaService, DisciplineTypeRepository, DisciplineRepository, DisciplineTeacherRepository, TeacherRepository, GroupRepository],
+  providers: [
+    PrismaService,
+    DisciplineTypeRepository,
+    DisciplineRepository,
+    DisciplineTeacherRepository,
+    DisciplineTeacherRoleRepository,
+    TeacherRepository,
+    GroupRepository,
+    SubjectRepository,
+    ScheduleRepository,
+  ],
+  exports: [
+    PrismaService,
+    DisciplineTypeRepository,
+    DisciplineRepository,
+    DisciplineTeacherRepository,
+    DisciplineTeacherRoleRepository,
+    TeacherRepository,
+    GroupRepository,
+    SubjectRepository,
+    ScheduleRepository,
+  ],
 })
 export class PrismaModule {}
