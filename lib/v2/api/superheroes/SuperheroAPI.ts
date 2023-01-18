d882d577e3589e5f1755aaafbb1e49d8410d6feb
miss-linetta <petrovych.nataliia@lll.kpi.ua>
2023-01-18 12:53:39 +0200

    superhero (last 2)

--- lib/v2/api/superheroes/SuperheroAPI.ts
+++ lib/v2/api/superheroes/SuperheroAPI.ts
@@ -1,1 +1,23 @@
-export class SuperheroAPI {}
+import { client, getAuthorizationHeader } from "../index";
+import { SuperheroVerificationBody } from "./dto/SuperheroVerificationBody";
+import { SendSuperheroRequestBody } from "./dto/SendSuperheroRequestBody";
+
+export class SuperheroAPI {
+
+    static async superheroVerification(accessToken: string, userId: string, state: string, body: SuperheroVerificationBody) {
+        return (await client.patch(
+            `/api/v2/user/superhero/verify/${userId}/${state}`,
+            body,
+            getAuthorizationHeader(accessToken)
+        ));
+    }
+
+    static async sendSuperheroRequest(accessToken: string, dorm: boolean, body: SendSuperheroRequestBody) {
+        return (await client.post(
+            `/api/v2/user/superhero/${dorm}`,
+            body,
+            getAuthorizationHeader(accessToken)
+        ));
+    }
+
+}
