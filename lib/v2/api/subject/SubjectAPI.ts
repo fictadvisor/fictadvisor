6e74d55ffa93b478a1e5ae2b046574d60cc7a6e1
mibal_ua <9mohapx9@gmail.com>
2023-01-19 10:04:08 +0100

    Implement `getTeachersBySubject` method

--- lib/v2/api/subject/SubjectAPI.ts
+++ lib/v2/api/subject/SubjectAPI.ts
@@ -1,5 +1,12 @@
 import { client, getAuthorizationHeader } from "../index";
 
+import { GetTeachersBySubjectDTO } from "./dto/GetTeachersBySubjectDTO";
+
 export class SubjectsAPI {
 
+    static async getTeachersBySubject(accessToken: string,
+                                      disciplineId: string): Promise<GetTeachersBySubjectDTO> {
+        return (await client.get(`disciplines/${disciplineId}/teachers`,
+            getAuthorizationHeader(accessToken))).data;
+    }
 }
