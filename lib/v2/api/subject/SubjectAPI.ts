64c1f59695fdfe94f03c00ed80f3cbacd2572787
mibal_ua <9mohapx9@gmail.com>
2023-01-19 10:10:50 +0100

    Implement `updateDiscipline` method

--- lib/v2/api/subject/SubjectAPI.ts
+++ lib/v2/api/subject/SubjectAPI.ts
@@ -1,6 +1,7 @@
 import { client, getAuthorizationHeader } from "../index";
 
 import { GetTeachersBySubjectDTO } from "./dto/GetTeachersBySubjectDTO";
+import { UpdateDisciplineBody } from "./dto/UpdateDisciplineBody";
 
 export class SubjectsAPI {
 
@@ -8,5 +9,13 @@ export class SubjectsAPI {
                                       disciplineId: string): Promise<GetTeachersBySubjectDTO> {
         return (await client.get(`disciplines/${disciplineId}/teachers`,
             getAuthorizationHeader(accessToken))).data;
+    }
+
+    static async updateDiscipline(accessToken: string,
+                                  disciplineId: string,
+                                  body: UpdateDisciplineBody) {
+        await client.patch(`disciplines/${disciplineId}`,
+            body,
+            getAuthorizationHeader(accessToken));
     }
 }
