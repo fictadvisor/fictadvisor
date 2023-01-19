329a74c1ad617ffbb06823bb34239707d03640dd
mibal_ua <9mohapx9@gmail.com>
2023-01-19 09:49:18 +0100

    Fix style and typo in ScheduleAPI.ts

--- lib/v2/api/schedule/ScheduleAPI.ts
+++ lib/v2/api/schedule/ScheduleAPI.ts
@@ -1,4 +1,5 @@
 import { client, getAuthorizationHeader } from "../index";
+
 import { GetSessionScheduleDTO } from "./dto/GetSessionScheduleDTO";
 import { GetScheduleDTO } from "./dto/GetScheduleDTO";
 import { GetDynamicLessonsDTO } from "./dto/GetDynamicLessonsDTO";
@@ -25,47 +26,56 @@ export class ScheduleAPI {
 
     static async getDynamicLessons(accessToken: string,
                                    id: string,
-                                   fortnight?: string): Promise<GetDynamicLessonsDTO>{
+                                   fortnight?: string): Promise<GetDynamicLessonsDTO> {
         return (await client.get(`schedule/groups/${id}/temporary/${fortnight}`,
             getAuthorizationHeader(accessToken))).data;
     }
-    
-     static async createLesson(accessToken: string, body: CreateLessonBody) {
-        return (await client.post(
-            `/schedule`,
+
+    static async createLesson(accessToken: string,
+                              body: CreateLessonBody) {
+        return (await client.post(`/schedule`,
             body,
             getAuthorizationHeader(accessToken)
         ));
     }
 
-    static async updateStaticLesson(accessToken: string, lessonId: string, body: UpdateStaticLessonBody) {
-        return (await client.patch(
-            `/schedule/lessons/static/${lessonId}`,
+    static async updateStaticLesson(accessToken: string,
+                                    lessonId: string,
+                                    body: UpdateStaticLessonBody) {
+        return (await client.patch(`/schedule/lessons/static/${lessonId}`,
             body,
             getAuthorizationHeader(accessToken)
         ));
     }
 
-    static async updateDynamicLesson(accessToken: string, id: string, fortnight: string, body: UpdateDynamicLessonBody) {
-        return (await client.patch(
-            `/schedule/lessons/static/${id}/${fortnight}}`,
+    static async updateDynamicLesson(accessToken: string,
+                                     id: string,
+                                     fortnight: string,
+                                     body: UpdateDynamicLessonBody) {
+        return (await client.patch(`/schedule/lessons/static/${id}/${fortnight}}`,
             body,
             getAuthorizationHeader(accessToken)
         ));
     }
 
-    static async getStaticLesson(accessToken: string, id: string, groupId: string): Promise<GetLessonDTO> {
-        return (await client.get(`/schedule/group/${groupId}/lessons/static/${id}`
-            , getAuthorizationHeader(accessToken))).data;
+    static async getStaticLesson(accessToken: string,
+                                 id: string,
+                                 groupId: string): Promise<GetLessonDTO> {
+        return (await client.get(`/schedule/group/${groupId}/lessons/static/${id}`,
+            getAuthorizationHeader(accessToken))).data;
     };
 
-    static async getTemporaryLesson(accessToken: string, id: string, groupId: string): Promise<GetLessonDTO> {
-        return (await client.get(`/schedule/group/${groupId}/lessons/temprorary/${id}`
-            , getAuthorizationHeader(accessToken))).data;
+    static async getTemporaryLesson(accessToken: string,
+                                    id: string,
+                                    groupId: string): Promise<GetLessonDTO> {
+        return (await client.get(`/schedule/group/${groupId}/lessons/temporary/${id}`,
+            getAuthorizationHeader(accessToken))).data;
     };
 
-    static async getTeacherSchedule(accessToken: string, teacherId: string, fortnight?: string): Promise<GetTeacherScheduleDTO> {
-        return (await client.get(`/schedule/teachers/${teacherId}/static/${fortnight}`
-            , getAuthorizationHeader(accessToken))).data;
+    static async getTeacherSchedule(accessToken: string,
+                                    teacherId: string,
+                                    fortnight?: string): Promise<GetTeacherScheduleDTO> {
+        return (await client.get(`/schedule/teachers/${teacherId}/static/${fortnight}`,
+            getAuthorizationHeader(accessToken))).data;
     }
 }
