ef593746378b38e701c96609ba05b739fd5b3d09
mibal_ua <9mohapx9@gmail.com>
2023-01-19 09:55:50 +0100

    Clear unnecessary methods

--- lib/v2/api/subject/SubjectAPI.ts
+++ lib/v2/api/subject/SubjectAPI.ts
@@ -1,23 +1,5 @@
-import { client, getAuthorizationHeader, QueryParams } from "../index";
-import { GetSubjectDTO } from "./dto/GetSubjectDTO";
-import { CreateSubjectBodyDTO } from "./dto/CreateSubjectBodyDTO";
+import { client, getAuthorizationHeader } from "../index";
 
 export class SubjectsAPI {
-  static async get(subjectId: string): Promise<GetSubjectDTO> {
-    return (await client.get(`/subjects/${subjectId}`)).data;
-  }
 
-  static async getAll(
-    params: QueryParams<"rating" | "name" | "teacherCount">
-  ): Promise<GetSubjectDTO[]> {
-    return (await client.get("/subjects", { params })).data;
-  }
-
-  static async create(accessToken: string, body: CreateSubjectBodyDTO) {
-    return await client.post(
-      `/subjects`,
-      body,
-      getAuthorizationHeader(accessToken)
-    );
-  }
 }
