0a4234286614e15842231ab7ca52c9efa26b9b92
Mykhailo Shylo <mshilo2004@gmail.com>
2023-01-19 11:07:23 +0200

    2 endpoints for Subject Added 2 new endpoints: 1. Delete subject 2. Get list of subjects

--- lib/v2/api/subject/SubjectAPI.ts
+++ lib/v2/api/subject/SubjectAPI.ts
@@ -1,6 +1,7 @@
 import { client, getAuthorizationHeader, QueryParams } from "../index";
 import { GetSubjectDTO } from "./dto/GetSubjectDTO";
 import { CreateSubjectBodyDTO } from "./dto/CreateSubjectBodyDTO";
+import { GetListOfSubjectsDTO } from "./dto/GetListOfSubjectsDTO";
 
 export class SubjectsAPI {
   static async get(subjectId: string): Promise<GetSubjectDTO> {
@@ -13,6 +14,14 @@ export class SubjectsAPI {
     return (await client.get("/subjects", { params })).data;
   }
 
+  static async getListOfSubjects(accessToken: string): Promise<GetListOfSubjectsDTO[]> {
+    return (await client.get(`/subjects`, getAuthorizationHeader(accessToken))).data;
+  }
+
+  static async deleteSubject(accessToken: string, subjectId: string) {
+    return (await client.delete(`/subjects/${subjectId}`, getAuthorizationHeader(accessToken)));
+  }
+
   static async create(accessToken: string, body: CreateSubjectBodyDTO) {
     return await client.post(
       `/subjects`,
@@ -21,3 +30,4 @@ export class SubjectsAPI {
     );
   }
 }
+
