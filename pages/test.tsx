efaba061f99f4e308d5a05c3980c578da40ab08d
miss-linetta <petrovych.nataliia@lll.kpi.ua>
2023-01-23 21:12:49 +0200

    changes

--- pages/test.tsx
+++ pages/test.tsx
@@ -8,43 +8,23 @@ function test(){
   return(
     <div id="tooltip">
         
-        <div className="tooltip-body" id="tooltip-default">
+        <div className="tooltip-body">
             <span>Tooltip Example</span>
         </div>
 
-        <div className="tooltip-body" id="tooltip-default">
+        <div className="tooltip-body">
             <span className="tooltip-text" id="tooltip-text-top">Tooltip Example</span>
         </div>
 
-        <div className="tooltip-body" id="tooltip-default">
+        <div className="tooltip-body">
             <span className="tooltip-text" id="tooltip-text-bottom">Tooltip Example</span>
         </div>
 
-        <div className="tooltip-body" id="tooltip-default">
+        <div className="tooltip-body">
             <span className="tooltip-text" id="tooltip-text-left">Tooltip Example</span>
         </div>
 
-        <div className="tooltip-body" id="tooltip-default">
-            <span className="tooltip-text" id="tooltip-text-right">Tooltip Example</span>
-        </div>
-
-        <div className="tooltip-body" id="tooltip-info">
-            <span>Tooltip Example</span>
-        </div>
-
-        <div className="tooltip-body" id="tooltip-info">
-            <span className="tooltip-text" id="tooltip-text-top">Tooltip Example</span>
-        </div>
-
-        <div className="tooltip-body" id="tooltip-info">
-            <span className="tooltip-text" id="tooltip-text-bottom">Tooltip Example</span>
-        </div>
-
-        <div className="tooltip-body" id="tooltip-info">
-            <span className="tooltip-text" id="tooltip-text-left">Tooltip Example</span>
-        </div>
-
-        <div className="tooltip-body" id="tooltip-info">
+        <div className="tooltip-body">
             <span className="tooltip-text" id="tooltip-text-right">Tooltip Example</span>
         </div>
 
