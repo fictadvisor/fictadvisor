d302c4e464db2cbb994c56bd38d267c7d284040b
igaryakqwe <igor.novykov@gmail.com>
2023-01-23 20:24:25 +0200

    updated

--- pages/test.tsx
+++ pages/test.tsx
@@ -10,45 +10,39 @@ function test(){
       <div className="white-link">
         <a href="">Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="blue-link">
         <a href="">Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="white-link-underlined">
         <a href="">Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="blue-link-underlined">
         <a href="">Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="arrow-white-link">
         <a href="">
           <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="white" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg> Click here to open documentation</a>
+        </svg>Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="arrow-blue-link">
         <a href="">
           <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="#0EA5E9" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg> Click here to open documentation</a>
+        </svg>
+          <span className="link-text">Click here to open documentation</span></a>
       </div>
-      <hr/>
       <div className="arrow-white-link-underlined">
         <a href="">
           <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="white" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg> Click here to open documentation</a>
+        </svg>Click here to open documentation</a>
       </div>
-      <hr/>
       <div className="arrow-blue-link-underlined">
         <a href="">
           <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="#0EA5E9" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg> Click here to open documentation</a>
+        </svg>Click here to open documentation</a>
       </div>
     </div>
 );
