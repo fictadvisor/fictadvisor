7a796d84eb5d7d8599a1b1533669953fd51a10aa
miss-linetta <petrovych.nataliia@lll.kpi.ua>
2023-01-23 14:08:24 +0200

    tooltip

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,60 +6,48 @@ type Props = OwnProps;
 function test(){
 
   return(
-    <div id="container">
-        <h1> This is header 1</h1>
-        <hr/>
-        <h2> This is header 2</h2>
-        <hr/>
-        <h3> This is header 3</h3>
-        <hr/>
-        <h4> This is header 4</h4>
-        <hr/>
-        <h5> This is header 5</h5>
-        <hr/>
-        <h6> This is header 6</h6>
-        <hr/>
-        <p className="body-secondary">Body Secondary</p>
-        <hr/>
-        <p className="body-primary">Body Primary</p>
-        <hr/>
-        <p className="overline-text">Overline</p>
-        <hr/>
-        <div className="button-container">
-              <button className="large-button"> Default </button>
-              <button className="large-button" disabled={true}> Disabled </button>
+    <div id="tooltip">
+        
+        <div className="tooltip-body" id="tooltip-default">
+            <span>Tooltip Example</span>
         </div>
-        <hr/>
-        <div className="button-container">
-            <button className="medium-button"> Default </button>
-            <button className="medium-button" disabled={true}> Disabled </button>
+
+        <div className="tooltip-body" id="tooltip-default">
+            <span className="tooltip-text" id="tooltip-text-top">Tooltip Example</span>
         </div>
-        <hr/>
-        <div className="button-container">
-            <button className="small-button">
-                Default
-            </button>
-            <button className="small-button" disabled={true}> Disabled </button>
+
+        <div className="tooltip-body" id="tooltip-default">
+            <span className="tooltip-text" id="tooltip-text-bottom">Tooltip Example</span>
         </div>
-        <hr/>
-        <form>
-            <div className="field">
-                <label> Label </label>
-                <input placeholder="Placeholder"/>
-            </div>
-            <div className="disabled-field">
-                <label> Label </label>
-                <input placeholder="Placeholder"/>
-            </div>
-            <div className="success-field">
-                <label> Label </label>
-                <input placeholder="Placeholder"/>
-            </div>
-            <div className="error-field">
-                <label> Label </label>
-                <input placeholder="Placeholder"/>
-            </div>
-        </form>
+
+        <div className="tooltip-body" id="tooltip-default">
+            <span className="tooltip-text" id="tooltip-text-left">Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-default">
+            <span className="tooltip-text" id="tooltip-text-right">Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-info">
+            <span>Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-info">
+            <span className="tooltip-text" id="tooltip-text-top">Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-info">
+            <span className="tooltip-text" id="tooltip-text-bottom">Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-info">
+            <span className="tooltip-text" id="tooltip-text-left">Tooltip Example</span>
+        </div>
+
+        <div className="tooltip-body" id="tooltip-info">
+            <span className="tooltip-text" id="tooltip-text-right">Tooltip Example</span>
+        </div>
+
     </div>
   );
 }
