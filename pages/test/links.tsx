766a0c4353286582a9ecf16fc56f56dfbff46788
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-01-24 09:06:26 +0200

    Minor fixes

--- pages/test/links.tsx
+++ pages/test/links.tsx
@@ -4,19 +4,19 @@ function links() {
     return (
         <div className="test-page-wrap">
             <div className="white-link">
-                <a href="pages">Click here to open documentation</a>
+                <a className="link-container" href="pages">Click here to open documentation</a>
             </div>
             <div className="blue-link">
-                <a href="pages">Click here to open documentation</a>
+                <a className="link-container" href="pages">Click here to open documentation</a>
             </div>
             <div className="white-link-underlined">
-                <a href="pages">Click here to open documentation</a>
+                <a className="link-container" href="pages">Click here to open documentation</a>
             </div>
             <div className="blue-link-underlined">
-                <a href="pages">Click here to open documentation</a>
+                <a className="link-container" href="pages">Click here to open documentation</a>
             </div>
             <div className="arrow-white-link">
-                <a href="pages">
+                <a className="link-container" href="pages">
                     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="white" stroke-width="1.2"
                               stroke-linecap="round" stroke-linejoin="round"/>
@@ -24,7 +24,7 @@ function links() {
                     Click here to open documentation</a>
             </div>
             <div className="arrow-blue-link">
-                <a href="pages">
+                <a className="link-container" href="pages">
                     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="#0EA5E9" stroke-width="1.2"
                               stroke-linecap="round" stroke-linejoin="round"/>
@@ -32,20 +32,20 @@ function links() {
                     <span className="link-text">Click here to open documentation</span></a>
             </div>
             <div className="arrow-white-link-underlined">
-                <a href="pages">
+                <a className="link-container" href="pages">
                     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="white" stroke-width="1.2"
                               stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    Click here to open documentation</a>
+                    <span className="link-text">Click here to open documentation</span></a>
             </div>
             <div className="arrow-blue-link-underlined">
-                <a href="pages">
+                <a className="link-container" href="pages">
                     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 5H19M5 9L1 5L5 9ZM1 5L5 1L1 5Z" stroke="#0EA5E9" stroke-width="1.2"
                               stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    Click here to open documentation</a>
+                    <span className="link-text">Click here to open documentation</span></a>
             </div>
         </div>);
 }
