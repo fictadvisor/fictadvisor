a8ceaaf070538479740a20eaf5460c7cd4421e9e
infectus <asdfchjolkjadsus1966@yahoo.com>
2023-01-25 22:42:00 +0200

    scrollbar styling for firefox users

--- pages/v2/test/textareas.tsx
+++ pages/v2/test/textareas.tsx
@@ -4,6 +4,7 @@ export default function textareas(){
     return(
         <div className='test-page-wrap'>
             <TextArea placeholder="Тиць..." />
+            <TextArea placeholder="Не тиць..." label="не тицькай"/>
             <TextArea placeholder="filler text" isDisabled={true} />
             <TextArea placeholder="huge" sizing={TextAreaSpecs.BIG}/>
             <TextArea placeholder="smoll" sizing={TextAreaSpecs.SMALL} label="srenck"/>
