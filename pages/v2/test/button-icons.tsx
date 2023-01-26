fe7ab757e5f66c3b6a4eb58bb47a8d5d3838ac75
igaryakqwe <igor.novykov@gmail.com>
2023-01-26 18:38:31 +0200

    updated

--- pages/v2/test/button-icons.tsx
+++ pages/v2/test/button-icons.tsx
@@ -4,76 +4,16 @@ function buttonIcons(){
     <div id="button-icons">
 
       <br/>
-      <div className="grey-button">
-        <button>Remove <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+      <div className="big-button">
+        <button>Remove <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
         </button>
       </div>
       <br/>
-      <div className="dark-red-button">
-        <button>Remove <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="red-button">
-        <button>Remove <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="darkest-red-button">
-        <button>Remove <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="disabled-button">
-        <button disabled>Remove <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 6L18 18M6 18L18 6L6 18Z" stroke="#343434" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-
-      <br/>
-      <br/>
-      <div className="grey-small-button">
-        <button>Add <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div className="small-button">
+        <button>Add <svg className="button-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M5 13L9 17L19 7" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="dark-green-button">
-        <button>Add <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 13L9 17L19 7" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="green-button">
-        <button>Add <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 13L9 17L19 7" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="darkest-green-button">
-        <button>Add <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 13L9 17L19 7" stroke="#FAFAFA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        </button>
-      </div>
-      <br/>
-      <div className="disabled-small-button">
-        <button disabled>Add <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 13L9 17L19 7" stroke="#343434" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-
         </button>
       </div>
     </div>
