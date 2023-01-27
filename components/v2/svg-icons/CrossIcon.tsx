82df2d460e30b11bbdf1847cfae227a71b2182e2
kujo205 <kuc8301@gmail.com>
2023-01-27 22:38:32 +0200

    completed alerts (what a hell)

---
+++ components/v2/svg-icons/CrossIcon.tsx
@@ -0,0 +1,19 @@
+export const CrossIcon = (props) => (
+  <svg
+    width="10"
+    height="10"
+    viewBox="0 0 10 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M1 9L9 1L1 9ZM1 1L9 9Z" fill="#737373" />
+    <path
+      d="M1 1L9 9M1 9L9 1L1 9Z"
+      stroke="white"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
