cf5e22d8e747306f8026eb755d1d68081d0083b3
Mykhailo Shylo <mshilo2004@gmail.com>
2023-01-28 21:57:08 +0200

    Add onClick

--- components/v2/Tab.tsx
+++ components/v2/Tab.tsx
@@ -13,9 +13,12 @@ export interface TabProps {
 }
 
 export function Tab(props: TabProps) {
+    const handleClick = () => {
+        
+    }
     return (
         <div>
-            <button className={props.className + " " + props.position} disabled={props.isDisabled}>
+            <button className={props.className + " " + props.position} disabled={props.isDisabled} onClick={handleClick}>
                 {props.icon ? <img className="icon" src={`/assets/icons/${props.icon}`}></img> : ""}
                 <text className="text">{props.text}</text>
                 {props.count ? <div className="count"><text className="count-text">{props.count}</text></div> : ""}
