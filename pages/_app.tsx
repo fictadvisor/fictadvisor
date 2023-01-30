7b3ab1a23ec7678a0c87f43113b6dac60adefd3b
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-01-31 00:29:33 +0200

    Refactored scss architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,3 @@
-// import "normalize.css/normalize.css";
-// import "../styles/v1/globals.css";
-// import "../styles/v1/input.css";
-// import "../styles/v1/layout/header.css";
-// import "../styles/v1/components/teacher.css";
-// import "../styles/v1/components/course.css";
-// import "../styles/v1/components/review.css";
-// import "../styles/v1/components/loader.css";
-// import "../styles/v1/components/statistics.css";
-// import "../styles/v1/components/contact.css";
-// import "../styles/v1/components/navigation.css";
-// import "../styles/v1/components/studentResource.css";
-// import "../styles/v1/components/article.css";
-// import "../styles/v1/sass/index.sass";
 import "../styles/v2/index.scss";
 
 import { QueryClient, QueryClientProvider } from "react-query";
