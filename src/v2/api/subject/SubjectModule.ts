016a6717f89d65700e29acbb774b404a32eb5484
just-hoshinon <mwadman2018@gmail.com>
2023-01-31 19:37:08 +0200

    fixed imports

--- src/v2/api/subject/SubjectModule.ts
+++ src/v2/api/subject/SubjectModule.ts
@@ -3,11 +3,12 @@ import { SubjectService } from './SubjectService';
 import { SubjectController } from './SubjectController';
 import { PrismaModule } from '../../database/PrismaModule';
 import { UserModule } from '../user/UserModule';
+import { DisciplineModule } from '../discipline/DisciplineModule';
 
 @Module({
   providers: [SubjectService],
   controllers: [SubjectController],
   exports: [SubjectService],
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, DisciplineModule],
 })
 export class SubjectModule {}
