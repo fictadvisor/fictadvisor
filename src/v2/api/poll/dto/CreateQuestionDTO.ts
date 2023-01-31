684171c01e848a74dcf82d7f1a84d40dd04f09bf
just-hoshinon <mwadman2018@gmail.com>
2023-01-31 20:03:35 +0200

    added validation

--- src/v2/api/poll/dto/CreateQuestionDTO.ts
+++ src/v2/api/poll/dto/CreateQuestionDTO.ts
@@ -1,13 +1,33 @@
-import { QuestionType, QuestionRole, QuestionAnswer } from '@prisma/client';
+import { QuestionType } from '@prisma/client';
+import { Type } from 'class-transformer';
+import { IsBoolean, IsEnum, IsNotEmpty, IsOptional, ValidateNested } from 'class-validator';
 export class CreateQuestionsDTO {
+
+    @ValidateNested({ each: true })
+    @Type(() => CreateQuestionData)
     questions: CreateQuestionData[];
 }
 
 export class CreateQuestionData {
+
+    @IsNotEmpty()
     category: string;
+
+    @IsEnum(QuestionType)
     type: QuestionType;
+
+    @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
     text: string;
+
+    @IsOptional()
     criteria?: string;
+
+    @IsOptional()
+    description?: string;
+
+    @IsBoolean()
     isRequired?: boolean;
 }
