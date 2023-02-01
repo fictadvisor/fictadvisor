2ca074268fd90a10333ae77d8fb3286593cf2cf6
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-01 13:39:38 +0200

    Finished inputs

--- components/v2/Input.tsx
+++ components/v2/Input.tsx
@@ -4,7 +4,7 @@ import {
     ExclamationCircleIcon,
     EyeIcon,
     EyeSlashIcon,
-    MagnifyingGlassIcon
+    MagnifyingGlassIcon, XMarkIcon
 } from "@heroicons/react/24/outline";
 
 export enum InputState{
@@ -34,11 +34,6 @@ interface InputProps{
 
 const Input: React.FC<InputProps> = (props) => {
 
-    const inputColor = `${props.type}-input-color `
-    const inputStyle = `${props.size}-${props.type}$-input`
-    const additionalClass = props.className ? " " + props.className : ""
-    const className = inputColor + inputStyle + additionalClass
-
     let remark;
     switch(props.state) {
         case InputState.DEFAULT: {
@@ -63,9 +58,15 @@ const Input: React.FC<InputProps> = (props) => {
     const fieldRef = useRef(null);
 
     function handleFocus(value: boolean){
-        setIsFocused(value)
-        if (isFocused) fieldRef.current.style.color = "#FFFFFF"
-        else           fieldRef.current.style.color = "inherit"
+        if (props.state === InputState.DEFAULT){
+            setIsFocused(value)
+            // if (isFocused) {
+            //     fieldRef.current.style.color = "#FFFFFF"
+            // }
+            // else{
+            //     fieldRef.current.style.color = "inherit"
+            // }
+        }
     }
 
     const [isHidden, setIsHidden] = useState(props.type === InputType.HIDDABLE);
@@ -76,20 +77,21 @@ const Input: React.FC<InputProps> = (props) => {
         setValue(event.target.value);
     }
 
-    const leftIcon = props.type === InputType.SEARCH ? <MagnifyingGlassIcon className="icon"/> : null;
+    const leftIcon = props.type === InputType.SEARCH ? <MagnifyingGlassIcon className="icon white-icon"/> : null;
 
     let rightIcon = null;
     if (props.type === InputType.HIDDABLE){
-        if (isHidden) rightIcon = <EyeSlashIcon className="icon"/>
-        else          rightIcon = <EyeIcon className="icon"/>
-    }
-    else if (props.type === InputType.SEARCH){
-            if (value !== null) rightIcon = <MagnifyingGlassIcon className="icon"/>
+        if (isHidden) rightIcon = <EyeIcon className="icon white-icon"/>
+        else          rightIcon = <EyeSlashIcon className="icon white-icon"/>
     }
     else{
         if (props.state === InputState.SUCCESS) rightIcon = <CheckCircleIcon className = "icon input-success-icon"/>
-        if (props.state === InputState.ERROR) rightIcon = <ExclamationCircleIcon className = "icon input-error-icon"/>
+        else if (props.state === InputState.ERROR) rightIcon = <ExclamationCircleIcon className = "icon input-error-icon"/>
+        else if (props.type === InputType.SEARCH){
+            if (value !== "") rightIcon = <XMarkIcon className="icon white-icon"/>
+        }
     }
+
 
     function handleIconClick(){
         if (props.type === InputType.HIDDABLE){
@@ -100,12 +102,18 @@ const Input: React.FC<InputProps> = (props) => {
         }
     }
 
+    const inputColor = `${props.state}-input-color `
+    const isIcon = props.state === InputState.ERROR || props.state === InputState.SUCCESS || props.type === InputType.HIDDABLE || (props.type === InputType.SEARCH && value !== "");
+    const inputStyle = `${props.size}-${props.type}${isIcon ? "-icon" : ""}-input`
+    const additionalClass = props.className ? " " + props.className : ""
+    const className = inputColor + inputStyle + additionalClass
+
 
     return (
         <div className={className}>
             <label ref={fieldRef}> {props.label} </label>
-            {leftIcon && <div className="icon leftIcon">leftIcon</div>}
-            {rightIcon && <div className="icon rightIcon" onClick={handleIconClick}>rightIcon</div>}
+            {leftIcon && <div className="icon left-icon">{leftIcon}</div>}
+            {rightIcon && <div className="icon right-icon" onClick={handleIconClick}>{rightIcon}</div>}
             <input placeholder={props.placeholder} type={inputType} onChange={handleChange} value = {value}
                    onFocus={() => handleFocus(true)}
                    onBlur={() => handleFocus(false)}/>
