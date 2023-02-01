bfd3deb08f7e06917103369238a689ff798d200a
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-01 15:14:31 +0200

    Fixed alerts

--- components/v2/custom-svg/CustomCheck.tsx
+++ components/v2/custom-svg/CustomCheck.tsx
@@ -1,15 +1,5 @@
 export const CustomCheck = (props) => (
-    <svg 
-    width="20" 
-    height="20" 
-    viewBox="0 0 20 20" 
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg" 
-    >
-    <path 
-    d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83334" 
-    stroke="white" 
-    stroke-linecap="round" 
-    stroke-linejoin="round"/>
-</svg>
+    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.5 13L9.5 17L19.5 7" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
 );
