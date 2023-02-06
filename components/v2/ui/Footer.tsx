e31dc90897ad14a5132b01a2d9d0561ab8d1ea9d
Mykhailo Shylo <mshilo2004@gmail.com>
2023-02-06 16:20:14 +0200

    Done Header

--- components/v2/ui/Footer.tsx
+++ components/v2/ui/Footer.tsx
@@ -1,10 +1,46 @@
-import React, {ReactNode} from "react";
-
+import React, { ReactNode } from "react";
+import styles from "styles/v2/local/components/Navigation.module.scss";
+import Button, { ButtonIconPosition, ButtonSize, ButtonType } from "./Button";
+import { AcademicCapIcon } from "@heroicons/react/24/outline";
 interface FooterProps {
+
 }
 
 const Footer: React.FC<FooterProps> = (props) => {
-
+    return (
+        <div className={styles["footer-container"]}>
+            <div className={styles["footer-logo"]}>
+                <img src={`/assets/logo.png`} alt="logo" />
+            </div>
+            <div className={styles["signature"]}>
+                <p>By Dev відділ ср ФІОТ</p>
+            </div>
+            <div className={styles["main-references"]}>
+                <div className={styles["title"]}>
+                    <p>Основні посилання</p>
+                </div>
+                <Button text="Головна" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="Опитування" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="Викладачі" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="Предмети" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="Розклад" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+            </div>
+            <div className={styles["support"]}>
+                <p className={styles["title"]}>
+                    Підтримка
+                </p>
+                <Button text="Конфіденційність" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="FICT robot" onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+            </div>
+            <div className={styles["social-media"]}>
+                <p className={styles["title"]}>
+                    Соціальне
+                </p>
+                <Button text="Git" icon={<AcademicCapIcon />} iconPosition={ButtonIconPosition.LEFT} onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+                <Button text="Tertiary" icon={<AcademicCapIcon />} iconPosition={ButtonIconPosition.LEFT} onClick={() => { }} isDisabled={false} size={ButtonSize.MEDIUM} type={ButtonType.TERTIARY} />
+            </div>
+        </div>
+    )
 }
 
 export default Footer;
