b09460505e523c9693c9024c91db9af45112fda1
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-10 19:48:55 +0200

    feat: removed npm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,14 @@ import "@/styles/reset.scss";
 import "@/styles/typography.scss";
 import "@/styles/global-styles.scss";
 
-import { QueryClient, QueryClientProvider } from "react-query";
+import {QueryClient, QueryClientProvider} from "react-query";
 
 const queryClient = new QueryClient();
 
-export default function Application({ Component, pageProps }) {
-  return (
+const Application = ({Component, pageProps}) => (
     <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
     </QueryClientProvider>
-  );
-}
+);
+
+export default Application;
