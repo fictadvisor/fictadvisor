8d6e284e8c129c00506f93da952ad1d7cd8a9d87
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-11 11:40:17 +0200

    fix: delete unneeded config file

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
-  extends: ['plugin:@next/next/recommended'],
-};
