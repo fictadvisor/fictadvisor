e0293791bf24c021879b74a62741230313bf3d5a
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-11 08:02:37 +0200

    feat: added ESLint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import "@/styles/reset.scss";
-import "@/styles/typography.scss";
-import "@/styles/global-styles.scss";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AppProps } from 'next/app';
 
-import {QueryClient, QueryClientProvider} from "react-query";
+import '@/styles/reset.scss';
+import '@/styles/typography.scss';
+import '@/styles/global-styles.scss';
 
 const queryClient = new QueryClient();
 
-const Application = ({Component, pageProps}) => (
-    <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-    </QueryClientProvider>
+const Application = ({ Component, pageProps }: AppProps) => (
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
 );
 
 export default Application;
