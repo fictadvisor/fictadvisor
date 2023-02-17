7995de835efd58f872e540fcd8a2da287e58adb3
kropli <dimakirimail@gmail.com>
2023-02-17 18:13:10 +0200

    Add repositories and modules

--- src/v2/api/resources/ResourceModule.ts
+++ src/v2/api/resources/ResourceModule.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../database/PrismaModule';
 import { ResourceService } from './ResourceService';
 import { ResourceController } from './ResourceController';
+import { UserModule } from '../user/UserModule';
 
 @Module({
   providers: [ResourceService],
   controllers: [ResourceController],
   exports: [ResourceService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
 })
 export class ResourceModule {}
