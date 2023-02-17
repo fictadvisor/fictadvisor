15dbf872f8e93a372c9e54068d153d4ae7cd02a0
kropli <dimakirimail@gmail.com>
2023-02-17 18:56:11 +0200

    Refactoring code

--- src/v2/api/resources/data/CreateResourceData.ts
+++ src/v2/api/resources/data/CreateResourceData.ts
@@ -1,24 +1,5 @@
-import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
-
 export class CreateResourceData {
-  @MinLength(3, {
-    message: 'name is too short (min: 3)',
-  })
-  @MaxLength(50, {
-    message: 'name is too long (max: 50)',
-  })
-  @IsNotEmpty({
-    message: 'name can not be empty',
-  })
   name: string;
-
-  @IsNotEmpty({
-    message: 'link can not be empty',
-  })
   link: string;
-
-  @IsNotEmpty({
-    message: 'icon can not be empty',
-  })
   icon: string;
 }
--- src/v2/api/resources/data/UpdateResourceData.ts
+++ src/v2/api/resources/data/UpdateResourceData.ts
@@ -1,18 +1,5 @@
-import { IsOptional, MaxLength, MinLength } from "class-validator";
-
 export class UpdateResourceData {
-  @MinLength(3, {
-    message: 'name is too short (min: 3)',
-  })
-  @MaxLength(50, {
-    message: 'name is too long (max: 50)',
-  })
-  @IsOptional()
   name: string;
-
-  @IsOptional()
   link: string;
-
-  @IsOptional()
   icon: string;
 }
