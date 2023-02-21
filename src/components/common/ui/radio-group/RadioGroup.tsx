de4e41f1a0099b2a27f779e313ef79568bb37ffc
Mykhailo Shylo <mshilo2004@gmail.com>
2023-02-21 18:14:43 +0200

    fix: refactored RadioGroup components

--- src/components/common/ui/radio-group/RadioGroup.tsx
+++ src/components/common/ui/radio-group/RadioGroup.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, ReactNode } from 'react';
+import React, { createContext, FC, ReactNode, useContext } from 'react';
+
+import styles from './RadioGroup.module.scss';
 
 interface RadioContextType {
   value: string;
@@ -34,4 +36,64 @@ const RadioGroup: React.FC<RadioGroupProps> = ({
     {children}
   </RadioContext.Provider>
 );
+
+interface RadioContextType {
+  value: string;
+  onChange: (value: string) => void;
+  name: string;
+  isDisabled?: boolean;
+}
+
+export enum RadioState {
+  DEFAULT = 'default',
+  ERROR = 'error',
+}
+
+interface RadioProps {
+  text: string;
+  value: string;
+  state?: RadioState;
+  isDisabled?: boolean;
+}
+
+export const Radio: FC<RadioProps> = ({
+  text,
+  value,
+  state = RadioState.DEFAULT,
+  isDisabled = false,
+}) => {
+  const {
+    value: currentValue,
+    name,
+    onChange,
+    isDisabled: GroupIsDisabled,
+  } = useContext(RadioContext);
+
+  const handleChange = () => {
+    onChange(value);
+  };
+
+  const additional = GroupIsDisabled || isDisabled ? '-disabled' : '';
+
+  return (
+    <div className={styles['radio-container']}>
+      <label>
+        <input
+          className={styles[state + '-radio-input' + `${additional}`]}
+          type="radio"
+          name={name}
+          value={value}
+          disabled={GroupIsDisabled || isDisabled}
+          checked={value === currentValue}
+          onChange={handleChange}
+        />
+        <span className={styles[state + '-radio-box' + `${additional}`]}></span>
+      </label>
+      <span className={styles[state + '-radio-text' + `${additional}`]}>
+        {text}
+      </span>
+    </div>
+  );
+};
+
 export default RadioGroup;
