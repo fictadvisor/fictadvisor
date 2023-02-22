6a4c8abba97ce1788ffdd0bec4f040f3b385a805
Mykhailo Shylo <mshilo2004@gmail.com>
2023-02-22 12:37:05 +0200

    feature: added mobile style for footer; uncompleted text for mobile version of social media

--- src/components/common/composite/footer/Footer.tsx
+++ src/components/common/composite/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import Link from 'next/link';
 
 import { GitHubIcon } from '@/components/common/custom-svg/GitHub';
@@ -13,119 +14,121 @@ import Button, {
 import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
+  const socialLabels =
+    window.innerWidth > 480
+      ? ['GitHub', 'Instagram', 'Telegram']
+      : ['', '', ''];
   return (
     <div className={styles['footer-container']}>
       <div>
         <div className={styles['footer-logo']}>
           <img src={`/assets/logo.png`} alt="logo" />
         </div>
+
         <div className={styles['signature']}>
           <p>By Dev відділ ср ФІОТ</p>
         </div>
       </div>
-      <div className={styles['link-container']}>
-        <div className={styles['main-references']}>
-          <div className={styles['title']}>
-            <p>Основні посилання</p>
-          </div>
-          <Link href={{}}>
-            <Button
-              text="Головна"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Опитування"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Викладачі"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Предмети"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Розклад"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
+      <div className={styles['main-references']}>
+        <div className={styles['title']}>
+          <p>Основні посилання</p>
         </div>
-        <div className={styles['support']}>
-          <div className={styles['title']}>
-            <p>Підтримка</p>
-          </div>
-          <Link href={{}}>
-            <Button
-              text="Конфіденційність"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="FICT robot"
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Головна"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Опитування"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Викладачі"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Предмети"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Розклад"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+      </div>{' '}
+      <div className={styles['support']}>
+        <div className={styles['title']}>
+          <p>Підтримка</p>
         </div>
-
-        <div className={styles['social-media']}>
-          <div className={styles['title']}>
-            <p>Соціальне</p>
-          </div>
-          <Link href={{}}>
-            <Button
-              text="GitHub"
-              icon={<GitHubIcon />}
-              iconPosition={ButtonIconPosition.LEFT}
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Instagram"
-              icon={<InstagramIcon />}
-              iconPosition={ButtonIconPosition.LEFT}
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
-          <Link href={{}}>
-            <Button
-              text="Telegram"
-              icon={<TelegramIcon />}
-              iconPosition={ButtonIconPosition.LEFT}
-              isDisabled={false}
-              size={ButtonSize.MEDIUM}
-              type={ButtonType.TERTIARY}
-            />
-          </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="Конфіденційність"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text="FICT robot"
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+      </div>
+      <div className={styles['social-media']}>
+        <div className={styles['title']}>
+          <p>Соціальне</p>
         </div>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text={socialLabels[0]}
+            icon={<GitHubIcon />}
+            iconPosition={ButtonIconPosition.LEFT}
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text={socialLabels[1]}
+            icon={<InstagramIcon />}
+            iconPosition={ButtonIconPosition.LEFT}
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
+        <Link href={{}} style={{ height: '36px' }}>
+          <Button
+            text={socialLabels[2]}
+            icon={<TelegramIcon />}
+            iconPosition={ButtonIconPosition.LEFT}
+            isDisabled={false}
+            size={ButtonSize.MEDIUM}
+            type={ButtonType.TERTIARY}
+          />
+        </Link>
       </div>
     </div>
   );
