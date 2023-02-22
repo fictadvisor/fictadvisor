ba5b14a9da66dde282df3b3e075828d6fc242ec2
Nikolai <nikolaichub.me@gmail.com>
2023-02-22 21:01:27 +0200

    feat: roz.kpi.ua parser

--- src/v2/utils/parser/ParserModule.ts
+++ src/v2/utils/parser/ParserModule.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleParser } from './ScheduleParser';
+import { RozParser } from './RozParser';
 import { PrismaModule } from '../../database/PrismaModule';
 
 @Module({
-  providers: [ScheduleParser],
-  exports: [ScheduleParser],
+  providers: [ScheduleParser, RozParser],
+  exports: [ScheduleParser, RozParser],
   imports: [PrismaModule],
 })
 export class ParserModule {}
