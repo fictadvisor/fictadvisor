28235a7f64da36ab660b05d4a9c9714e219e6776
HJK <supermaster98z@gmail.com>
2023-02-24 16:53:11 +0000

    feature: added clouds

--- src/components/pages/404-page/NotFoundPage.tsx
+++ src/components/pages/404-page/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 import { ChevronLeftIcon } from '@heroicons/react/24/outline';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
-import { CustomCheck } from '@/components/common/custom-svg/CustomCheck';
 import AlertButton, {
   AlertButtonVariant,
 } from '@/components/common/ui/alert-button';
@@ -12,40 +12,38 @@ import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => (
   <PageLayout description={'Сторінку не знайдено'}>
-    <div className={styles['not-found-page']}>
-      <nav className={styles['navigation']}></nav>
-      <div className={styles['main-info']}>
-        <div className={styles['main-content']}>
-          <h5 className={styles['not-found-text']}>
-            Упс! Сторінку не знайдено. Жабка з’їла твою сторінку
-          </h5>
-          <img
-            src="/assets/404-page/404_and_frog.svg"
-            className={styles['not-found-image']}
-            alt="Це 404"
-          />
-          <div className={styles['not-found-button']}>
-            <Button
-              text={'Повернутися на голову'}
-              size={ButtonSize.LARGE}
-              startIcon={<ChevronLeftIcon className={'icon'} />}
-            />
-          </div>
-          <div className={styles['not-found-button-mobile']}>
-            <Button
-              text={'Повернутися на голову'}
-              size={ButtonSize.SMALL}
-              startIcon={<ChevronLeftIcon className={'icon'} />}
-            />
-          </div>
-        </div>
-        <div className={styles['not-found-alert-button']}>
-          <AlertButton
-            text={"Зв'жіться з адміністратором"}
-            variant={AlertButtonVariant.ERROR_FILLED}
-            startIcon={<CustomCheck />}
+    <div className={styles['page-layout']}>
+      <nav className={styles['navbar']}></nav>
+      <div className={styles['page-content']}>
+        <h5 className={styles['not-found-text']}>
+          Упс! Сторінку не знайдено. Жабка з’їла твою сторінку
+        </h5>
+        <img
+          src="/assets/404-page/404_and_frog.svg"
+          className={styles['frog-image']}
+          alt="Це 404"
+        />
+        <div className={styles['button-desktop']}>
+          <Button
+            text={'Повернутися на голову'}
+            size={ButtonSize.LARGE}
+            startIcon={<ChevronLeftIcon className={'icon'} />}
           />
         </div>
+        <div className={styles['button-mobile']}>
+          <Button
+            text={'Повернутися на голову'}
+            size={ButtonSize.SMALL}
+            startIcon={<ChevronLeftIcon className={'icon'} />}
+          />
+        </div>
+      </div>
+      <div className={styles['alert-button']}>
+        <AlertButton
+          text={"Звя'жіться з адміністратором"}
+          variant={AlertButtonVariant.ERROR_FILLED}
+          startIcon={<ExclamationTriangleIcon className={'icon'} />}
+        />
       </div>
     </div>
   </PageLayout>
