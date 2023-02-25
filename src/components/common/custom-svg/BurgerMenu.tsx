bc38f3571af39242982f151d4986a93c24a5340e
Mykhailo Shylo <mshilo2004@gmail.com>
2023-02-25 21:16:03 +0200

    feature: added new icons

---
+++ src/components/common/custom-svg/BurgerMenu.tsx
@@ -0,0 +1,17 @@
+export const BurgerMenu = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4 18H20M4 6H20H4ZM4 12H20H4Z"
+      stroke="#FAFAFA"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ src/components/common/custom-svg/XMark.tsx
@@ -0,0 +1,17 @@
+export const XMark = () => (
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1L13 13M1 13L13 1L1 13Z"
+      stroke="#FAFAFA"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
