510d2a3bd7f7ae5b83c06b2a98ad9a8aa1e50297
just-hoshinon <mwadman2018@gmail.com>
2023-02-25 02:41:40 +0200

    fix: minor fixes of messages

--- src/v2/api/discipline/dto/CreateDisciplineTypeDTO.ts
+++ src/v2/api/discipline/dto/CreateDisciplineTypeDTO.ts
@@ -3,7 +3,7 @@ import { validationOptionsMsg } from '../../../utils/GLOBALS';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class CreateDisciplineTypeDTO {
-  @IsNotEmpty(validationOptionsMsg('DisciplineId can not be empty'))
+  @IsNotEmpty(validationOptionsMsg('Discipline id can not be empty'))
     disciplineId: string;
 
   @IsEnum(DisciplineTypeEnum, validationOptionsMsg('Name is not an enum'))
