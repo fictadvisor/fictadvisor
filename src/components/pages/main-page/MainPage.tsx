24030add791c828edc6258c6dbc2522a15aeadde
miss-linetta <petrovych.nataliia@lll.kpi.ua>
2023-02-28 22:48:00 +0200

    feature: added main page

--- src/components/pages/main-page/MainPage.tsx
+++ src/components/pages/main-page/MainPage.tsx
@@ -26,59 +26,61 @@ const MainPage = () => {
         <div className={styles['main-page-content']}>
           <div className={styles['header']}>
             <div className={styles['header-info']}>
-              <h2>Твій студентський портал</h2>
-              <p>
-                Мега текст що розповість про проект, що на ньому буде і тд і ще
-                текст і ще текст і ще текст і ще текст і ще текст і ще текст і
-                ще текст і ще текст і ще текст і ще текст
-              </p>
-              <div className={styles['buttons']}>
-                <div className={styles['buttons-desk']}>
-                  <Button
-                    text="Доєднатись"
-                    disabled={false}
-                    color={ButtonColor.PRIMARY}
-                    variant={ButtonVariant.FILLED}
-                    size={ButtonSize.LARGE}
-                  />
-                  <hr className={styles['button-divider']} />
-                  <Button
-                    text={'Пройти Опитування 2022'}
-                    disabled={false}
-                    variant={ButtonVariant.OUTLINE}
-                    size={ButtonSize.LARGE}
-                  />
-                </div>
-                <div className={styles['buttons-tabl']}>
-                  <Button
-                    text="Доєднатись"
-                    disabled={false}
-                    color={ButtonColor.PRIMARY}
-                    variant={ButtonVariant.FILLED}
-                    size={ButtonSize.MEDIUM}
-                  />
-                  <hr className={styles['button-divider']} />
-                  <Button
-                    text={'Пройти Опитування 2022'}
-                    disabled={false}
-                    variant={ButtonVariant.OUTLINE}
-                    size={ButtonSize.MEDIUM}
-                  />
-                </div>
-                <div className={styles['buttons-mob']}>
-                  <Button
-                    text="Доєднатись"
-                    disabled={false}
-                    color={ButtonColor.PRIMARY}
-                    variant={ButtonVariant.FILLED}
-                    size={ButtonSize.SMALL}
-                  />
-                  <Button
-                    text={'Опитування 2022'}
-                    disabled={false}
-                    variant={ButtonVariant.OUTLINE}
-                    size={ButtonSize.SMALL}
-                  />
+              <div className={styles['header-info-content']}>
+                <h2>Твій студентський портал</h2>
+                <p>
+                  Мега текст що розповість про проект, що на ньому буде і тд і
+                  ще текст і ще текст і ще текст і ще текст і ще текст і ще
+                  текст і ще текст і ще текст і ще текст і ще текст
+                </p>
+                <div className={styles['buttons']}>
+                  <div className={styles['buttons-desk']}>
+                    <Button
+                      text="Доєднатись"
+                      disabled={false}
+                      color={ButtonColor.PRIMARY}
+                      variant={ButtonVariant.FILLED}
+                      size={ButtonSize.LARGE}
+                    />
+                    <hr className={styles['button-divider']} />
+                    <Button
+                      text={'Пройти Опитування 2022'}
+                      disabled={false}
+                      variant={ButtonVariant.OUTLINE}
+                      size={ButtonSize.LARGE}
+                    />
+                  </div>
+                  <div className={styles['buttons-tabl']}>
+                    <Button
+                      text="Доєднатись"
+                      disabled={false}
+                      color={ButtonColor.PRIMARY}
+                      variant={ButtonVariant.FILLED}
+                      size={ButtonSize.MEDIUM}
+                    />
+                    <hr className={styles['button-divider']} />
+                    <Button
+                      text={'Пройти Опитування 2022'}
+                      disabled={false}
+                      variant={ButtonVariant.OUTLINE}
+                      size={ButtonSize.MEDIUM}
+                    />
+                  </div>
+                  <div className={styles['buttons-mob']}>
+                    <Button
+                      text="Доєднатись"
+                      disabled={false}
+                      color={ButtonColor.PRIMARY}
+                      variant={ButtonVariant.FILLED}
+                      size={ButtonSize.SMALL}
+                    />
+                    <Button
+                      text={'Опитування 2022'}
+                      disabled={false}
+                      variant={ButtonVariant.OUTLINE}
+                      size={ButtonSize.SMALL}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -88,7 +90,70 @@ const MainPage = () => {
           </div>
           <div className={styles['resources']}>
             <h3>Студентські ресурси</h3>
-            <div className={styles['resources-cards']}>
+            <div className={styles['resource-card-container']}>
+              <div className={styles['resources-cards']}>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+                <div className={styles['card-holder']}>
+                  <ResourceCard
+                    text={'FICT Time'}
+                    image={'/assets/resource.png'}
+                  />
+                </div>
+              </div>
+              {/* <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
@@ -96,8 +161,7 @@ const MainPage = () => {
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
               <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
-              <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
-              <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} />
+              <ResourceCard text={'FICT Time'} image={'/assets/resource.png'} /> */}
             </div>
           </div>
         </div>
