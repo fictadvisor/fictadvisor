e89077e145cfe413b891bf048b2f45c98c735013
Dimdim28 <wwwdimanes1@gmail.com>
2023-03-01 20:51:03 +0200

    feature: added answers sheet form

--- src/components/pages/poll-page/components/answers-sheet/AnswersSheet.tsx
+++ src/components/pages/poll-page/components/answers-sheet/AnswersSheet.tsx
@@ -1,6 +1,11 @@
 // import { Slider } from '@/components/common/ui/form';
 // import { Formik, Form } from 'formik';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
+
+import Button from '@/components/common/ui/button/Button';
+import { RadioGroup, Slider, TextArea } from '@/components/common/ui/form';
 
 import { Category } from '../../PollPage';
 
@@ -14,22 +19,81 @@ interface AnswersSheetProps {
 
 const AnswersSheet: React.FC<AnswersSheetProps> = ({ questions }) => {
   console.log(questions);
+  const initialValues = {};
+
+  for (const question of questions.questions) {
+    if (question.type === 'SCALE') initialValues[question.id] = 1;
+    console.log('initial', initialValues);
+  }
+  const handleSubmit = data => {
+    console.log('hello', data);
+  };
+  const validationSchema = yup.object().shape({});
+
+  useEffect(() => {
+    for (const question of questions.questions) {
+      if (question.type === 'SCALE') initialValues[question.id] = 1;
+    }
+    console.log('initial', initialValues);
+  }, [questions, initialValues]);
+
   return (
     <div className={styles.wrapper}>
-      {/* <Formik
-          enableReinitialize
-          validateOnMount
-          validateOnChange
-          initialValues={initialValues}
-          onSubmit={handleSubmit}
-          validationSchema={validationSchema}
-        >
-           {() => (
-            <Form className={styles['form']}>
+      <Formik
+        enableReinitialize
+        validateOnMount
+        validateOnChange
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        {() => (
+          <Form className={styles['form']}>
+            {questions.questions.map((question, id) => (
+              <div key={id} className={styles['question']}>
+                {question.type === 'TEXT' ? (
+                  <p className={styles['question-number']}> Відкрите питання</p>
+                ) : (
+                  <p className={styles['question-number']}>
+                    Питання {id + 1} / {questions.count}
+                  </p>
+                )}
 
-<Slider name={'rating'} />
-            </Form>
-                    )} */}
+                <b className={styles['question-title']}>{question.name}</b>
+                {question.description && (
+                  <p className={styles['question-description']}>
+                    {question.description}
+                  </p>
+                )}
+                {question.type === 'SCALE' ? (
+                  <Slider className={styles['slider']} name={question.id} />
+                ) : question.type === 'TOGGLE' ? (
+                  <RadioGroup
+                    className={styles['options']}
+                    options={[
+                      { value: '1', label: 'так' },
+                      { value: '0', label: 'ні' },
+                    ]}
+                    name={question.id}
+                  />
+                ) : (
+                  <TextArea className={styles['textarea']} name={question.id} />
+                )}
+                {question.criteria && (
+                  <p className={styles['question-criteria']}>
+                    {question.criteria}
+                  </p>
+                )}
+              </div>
+            ))}
+            <Button
+              className={styles['button']}
+              text="Наступні питання"
+              type="submit"
+            />
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
