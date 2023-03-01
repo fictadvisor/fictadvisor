fdd4da32abfeb0f927b0fea27ee30c47ec0df849
Markusplay <mshilo2004@gmail.com>
2023-03-01 10:07:57 +0200

    fix: fixed flexability

--- src/components/common/ui/alert-popup/AlertPopup.tsx
+++ src/components/common/ui/alert-popup/AlertPopup.tsx
@@ -1,14 +1,39 @@
 //margin right 80px margin bottom 36px
-import React, { ReactNode } from 'react';
+import React from 'react';
+
+import Alert, { AlertColor, AlertVariant } from '../alert/Alert';
 
 import styles from './AlertPopup.module.scss';
 
-interface AlertPopupProps {
-  children: ReactNode;
+export interface AlertPopupProps {
+  title: string;
+  description?: string;
+  color?: AlertColor;
+  variant?: AlertVariant;
+  isClosable?: boolean;
+  className?: string;
 }
 
-const AlertPopup: React.FC<AlertPopupProps> = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
+const AlertPopup: React.FC<AlertPopupProps> = ({
+  title,
+  description,
+  color,
+  variant,
+  isClosable,
+  className,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <Alert
+        className={className}
+        isClosable={isClosable}
+        title={title}
+        description={description}
+        variant={variant}
+        color={color}
+      />
+    </div>
+  );
 };
 
 export default AlertPopup;
