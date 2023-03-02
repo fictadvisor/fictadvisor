2c535b1ec8b5f6d6a94e4b06c44f3cd1e0266252
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-03-02 15:52:53 +0200

    Changes by MWadman

--- src/components/pages/register/register-page/components/register-form/validation/index.ts
+++ src/components/pages/register/register-page/components/register-form/validation/index.ts
@@ -1,12 +1,50 @@
 import * as yup from 'yup';
 
 export const validationSchema = yup.object().shape({
-  username: yup.string().required(`Обов'язкове поле`),
-  firstName: yup.string().required(`Обов'язкове поле`),
-  lastName: yup.string().required(`Обов'язкове поле`),
-  email: yup.string().required(`Обов'язкове поле`),
+  username: yup
+    .string()
+    .required(`Обов'язкове поле`)
+    .min(2, 'Не коротше 2 символів')
+    .max(40, 'Не довше 40 символів')
+    .matches(/^\w+$/, 'Має містити латинські літери, цифри або знак _'),
+  firstName: yup
+    .string()
+    .required(`Обов'язкове поле`)
+    .min(2, 'Не коротше 2 символів')
+    .max(40, 'Не довше 40 символів')
+    .matches(
+      /^[ҐЄІЇЬА-ЩЮЯґєіїьа-щюя\-' ]+$/,
+      'Має містити українські літери, апостроф або дефіс',
+    ),
+  lastName: yup
+    .string()
+    .required(`Обов'язкове поле`)
+    .min(2, 'Не коротше 2 символів')
+    .max(40, 'Не довше 40 символів')
+    .matches(
+      /^[ҐЄІЇЬА-ЩЮЯґєіїьа-щюя\-' ]+$/,
+      'Має містити українські літери, апостроф або дефіс',
+    ),
+  middleName: yup
+    .string()
+    .min(2, 'Не коротше 2 символів')
+    .max(40, 'Не довше 40 символів')
+    .matches(
+      /^[ҐЄІЇЬА-ЩЮЯґєіїьа-щюя\-' ]+$/,
+      'Має містити українські літери, апостроф або дефіс',
+    ),
+  email: yup
+    .string()
+    .required(`Обов'язкове поле`)
+    .email('Це не схоже на пошту'),
   group: yup.string().required(`Обов'язкове поле`),
-  password: yup.string().required(`Обов'язкове поле`),
+  password: yup
+    .string()
+    .required(`Обов'язкове поле`)
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[\w\W]+$/,
+      'Мінімум одна літера та одна цифра',
+    ),
   passwordConfirmation: yup
     .string()
     .oneOf([yup.ref('password'), null], 'Паролі не збігаються')
