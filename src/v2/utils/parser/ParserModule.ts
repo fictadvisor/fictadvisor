9072ecda18b517d15f275cb30e740d46a5aeec20
just-hoshinon <mwadman2018@gmail.com>
2023-03-02 04:42:52 +0200

    fix: fixed module imports

--- src/v2/utils/parser/ParserModule.ts
+++ src/v2/utils/parser/ParserModule.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScheduleParser } from './ScheduleParser';
 import { RozParser } from './RozParser';
 import { PrismaModule } from '../../database/PrismaModule';
+import { GroupModule } from '../../api/group/GroupModule';
 
 @Module({
   providers: [ScheduleParser, RozParser],
   exports: [ScheduleParser, RozParser],
-  imports: [PrismaModule],
+  imports: [PrismaModule, GroupModule],
 })
 export class ParserModule {}
