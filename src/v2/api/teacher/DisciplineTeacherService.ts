fictadvisor/fictadvisor-api

enhancement #286: Made system determine current semester
- Now the information about current semester is not taken from the config file but from the database.

- If the semester was not found, a new error will be thrown from the DataNotFoundException.ts file.

closes #286

src/v2/api/teacher/DisciplineTeacherService.ts
@@ -169,7 +169,7 @@ export class DisciplineTeacherService {
   }
 
   async getPollTimeBorders () {
-    const { year, semester } = await this.dateService.getCurrentYearAndSemester();
+    const { year, semester } = await this.dateService.getCurrentYear_Semester_StartDate();
     const startPoll = await this.dateService.getDateVar(`START_POLL_${year}_${semester}`);
     const endPoll = await this.dateService.getDateVar(`END_POLL_${year}_${semester}`);
     return {

fictadvisor/fictadvisor

src/v2/api/teacher/DisciplineTeacherService.ts
@@ -169,7 +169,7 @@ export class DisciplineTeacherService {
   }
 
   async getPollTimeBorders () {
-    const { year, semester } = await this.dateService.getCurrentYearAndSemester();
+    const { year, semester } = await this.dateService.getCurrentSemester();
     const startPoll = await this.dateService.getDateVar(`START_POLL_${year}_${semester}`);
     const endPoll = await this.dateService.getDateVar(`END_POLL_${year}_${semester}`);
     return {