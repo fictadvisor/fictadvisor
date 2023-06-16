a1f7be9f2a4beffe46ab05f2057b23cc24f12f42
Ivan Kuts <86970828+kujo205@users.noreply.github.com>
2023-06-16 14:30:44 +0300

    added ssg to RegisterPage.tsx (#406)

--- src/components/pages/register/register-page/RegisterPage.tsx
+++ src/components/pages/register/register-page/RegisterPage.tsx
@@ -1,6 +1,6 @@
-import { useQuery } from 'react-query';
+import type { FC } from 'react';
 
-import { GroupAPI } from '@/lib/api/group/GroupAPI';
+import type { GetAllDTO } from '@/lib/api/group/dto/GetAllDTO';
 
 import PageLayout from '../../../common/layout/page-layout/PageLayout';
 
@@ -9,11 +9,11 @@ import RightBlock from './components/right-block';
 
 import styles from './RegisterPage.module.scss';
 
-const RegisterPage = () => {
-  const { isLoading, data } = useQuery(['groups'], GroupAPI.getAll, {
-    refetchOnWindowFocus: false,
-  });
+export interface RegisterPageProps {
+  data: GetAllDTO;
+}
 
+const RegisterPage: FC<RegisterPageProps> = ({ data }) => {
   return (
     <PageLayout
       description={'Сторінка для авторизації'}
@@ -22,13 +22,9 @@ const RegisterPage = () => {
     >
       <div className={styles['register-page']}>
         <div className={styles['register-page__content']}>
-          {!isLoading && (
-            <>
-              <LeftBlock groups={data.groups || []} />
-              <hr className={styles['divider']} />
-              <RightBlock />
-            </>
-          )}
+          <LeftBlock groups={data.groups || []} />
+          <hr className={styles['divider']} />
+          <RightBlock />
         </div>
       </div>
     </PageLayout>
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,27 @@
-import RegisterPage from '@/components/pages/register/register-page';
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-const Register = () => <RegisterPage />;
+import RegisterPage from '@/components/pages/register/register-page';
+import type { RegisterPageProps } from '@/components/pages/register/register-page/RegisterPage';
+import { GroupAPI } from '@/lib/api/group/GroupAPI';
+
+export const getStaticProps: GetStaticProps<RegisterPageProps> = async () => {
+  let data: RegisterPageProps['data'];
+
+  try {
+    data = await GroupAPI.getAll();
+  } catch (error: unknown) {
+    data = null;
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const Register = (props: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <RegisterPage {...props} />
+);
 
 export default Register;
