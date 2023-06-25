2278bfa2cb86d32c88995f7306e990d1d31e88c0
Michael Shylo <102873980+Markusplay@users.noreply.github.com>
2023-06-25 12:14:13 +0300

    bugfix: added back cover when all polls are completed

--- src/components/pages/search-pages/poll-teachers-page/PollTeacherSearchList.tsx
+++ src/components/pages/search-pages/poll-teachers-page/PollTeacherSearchList.tsx
@@ -1,8 +1,13 @@
-import React, { FC, useEffect } from 'react';
+import { FC } from 'react';
+import { Box, Typography, useMediaQuery } from '@mui/material';
+import Image from 'next/image';
 
 import { PollTeacherCard } from '@/components/common/ui/cards/poll-teacher-card';
-import useToast from '@/hooks/use-toast';
+import useAuthentication from '@/hooks/use-authentication';
 import { PollTeachersDTO } from '@/lib/api/poll/dto/PollTeachersDTO';
+import theme from '@/styles/theme';
+
+import * as stylesMUI from './PollTeacherSearchList.styles';
 
 import styles from './PollTeacherSearchList.module.scss';
 
@@ -15,30 +20,50 @@ const PollTeacherSearchList: FC<PollTeacherSearchListProps> = ({
   data,
   className,
 }) => {
-  const toast = useToast();
-  useEffect(() => {
-    if (data.teachers?.length === 0) {
-      toast.success(
-        'Всі опитування вже пройдені!',
-        `Ви супермолодчинка! Ваша швидкість більша ніж 299 792 км/год!`,
-      );
-    }
-  }, []);
+  const { user } = useAuthentication();
+  const isMobile = useMediaQuery(theme.breakpoints.down('tablet'));
+
+  const isCompleted = data.teachers?.length === 0;
+  const groupName = user?.group.code;
   return (
-    <ul className={styles[`${className}-search-list`]}>
-      {data &&
-        data.teachers?.map(teacher => (
-          <PollTeacherCard
-            id={teacher.disciplineTeacherId}
-            key={teacher.disciplineTeacherId}
-            description={teacher.subject.name}
-            avatar={teacher.avatar}
-            name={`${teacher.lastName} ${teacher.firstName} ${teacher.middleName}`}
-            roles={teacher.roles}
-            href={`/poll/${teacher.disciplineTeacherId}`}
-          />
-        ))}
-    </ul>
+    <>
+      <Typography sx={stylesMUI.headText}>
+        Викладачі предметів групи {groupName}
+      </Typography>
+      {isCompleted && (
+        <Box sx={stylesMUI.wrapper}>
+          <Box sx={stylesMUI.content}>
+            <Image
+              src="/gifs/frog-complete.gif"
+              alt="Frogs complete the poll"
+              width={isMobile ? 300 : 480}
+              height={isMobile ? 125 : 200}
+              quality={100}
+            />
+            <Typography sx={stylesMUI.headText}>На цей семестр всьо</Typography>
+            <Typography variant="body2">
+              Ти вже пройшов опитування за всіх викладачів, що викладали в тебе
+              в цьому семестрі, дочекайся наступного семестру, аби знов залишити
+              відгук. Сподіваємось ти добре закрив сесію!
+            </Typography>
+          </Box>
+        </Box>
+      )}
+      <ul className={styles[`${className}-search-list`]}>
+        {data &&
+          data.teachers?.map(teacher => (
+            <PollTeacherCard
+              key={teacher.disciplineTeacherId}
+              description={teacher.subject.name}
+              avatar={teacher.avatar}
+              name={`${teacher.lastName} ${teacher.firstName} ${teacher.middleName}`}
+              roles={teacher.roles}
+              href={`/poll/${teacher.disciplineTeacherId}`}
+              id={teacher.disciplineTeacherId}
+            />
+          ))}
+      </ul>
+    </>
   );
 };
 
