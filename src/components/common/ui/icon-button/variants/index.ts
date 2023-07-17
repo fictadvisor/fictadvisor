3f2a9120c56797c2dfdfc903f4b8be014ff84bb9
Markusplay <mshilo2004@gmail.com>
2023-07-17 19:38:42 +0300

    enhancement: removed deprecated component`CustomDivider`

--- src/components/common/ui/icon-button/variants/index.ts
+++ src/components/common/ui/icon-button/variants/index.ts
@@ -1,4 +1,1 @@
-export { default as CloseButton } from './CloseButton';
-export { default as SortButton, SortButtonOrder } from './SortButton';
-export { default as StarButton } from './StarButton';
 export { default as TrashBucketButton } from './TrashBucketButton';
--- src/components/pages/account-page/components/general-tab/GeneralTab.tsx
+++ src/components/pages/account-page/components/general-tab/GeneralTab.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { useRouter } from 'next/router';
 
 import { TelegramForAccount } from '@/components/common/icons/TelegramForAccount';
@@ -7,7 +6,8 @@ import Button, {
   ButtonSize,
   ButtonVariant,
 } from '@/components/common/ui/button';
-import CustomDivider from '@/components/pages/account-page/components/divider';
+import Divider from '@/components/common/ui/divider-mui';
+import { DividerTextAlign } from '@/components/common/ui/divider-mui/types';
 import ContactsBlock from '@/components/pages/account-page/components/general-tab/components/contacts-block/ContactsBlock';
 import PersonalInfoBlock from '@/components/pages/account-page/components/general-tab/components/personal-info';
 import useAuthentication from '@/hooks/use-authentication';
@@ -31,7 +31,11 @@ const GeneralTab: FC = () => {
     <div className={styles['container']}>
       <div className={styles['personal-info']}>
         <PersonalInfoBlock />
-        <CustomDivider text="Посилання на соц. мережі" />
+        <Divider
+          sx={{ paddingBottom: '20px' }}
+          textAlign={DividerTextAlign.LEFT}
+          text="Посилання на соц. мережі"
+        />
         <ContactsBlock />
       </div>
       <div className={styles['avatar-and-telegram-info']}>
