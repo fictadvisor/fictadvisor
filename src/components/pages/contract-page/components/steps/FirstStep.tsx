e43cd560d8d2ad1f1b4791e81f3253b0185ffe36
kujo205 <kuc8301@gmail.com>
2023-08-06 13:12:42 +0300

    added margins between checkboxes

--- src/components/pages/contract-page/components/steps/FirstStep.tsx
+++ src/components/pages/contract-page/components/steps/FirstStep.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useRef } from 'react';
 import { Box, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 import { Form, Formik, FormikProps } from 'formik';
 
 import FormikRadioGroup from '@/components/common/ui/form/with-formik/radio/FormikRadioGroup';
@@ -51,88 +52,92 @@ export const FirstStep: FC<FirstStepProps> = ({
     >
       {({ values, isValid, touched }) => (
         <Form>
-          <Box sx={stylesMui.item}>
-            <Typography variant="h6Bold">
-              Форма навчання (бюджет/контракт)
-            </Typography>
-            <FormikRadioGroup
-              name="meta.studyType"
-              options={[
-                { label: 'Бюджет', value: StudyTypeParam.BUDGET },
-                { label: 'Контракт', value: StudyTypeParam.CONTRACT },
-              ]}
-            />
-          </Box>
-          {values.meta.studyType === StudyTypeParam.CONTRACT && (
+          <Stack gap={'20px'}>
             <Box sx={stylesMui.item}>
               <Typography variant="h6Bold">
-                Оплата(Щосеместрово/Щоквартально)
+                Форма навчання (бюджет/контракт)
               </Typography>
               <FormikRadioGroup
-                name="meta.paymentType"
+                name="meta.studyType"
                 options={[
-                  {
-                    value: PaymentTypeParam.EVERY_SEMESTER,
-                    label: 'Щосеместрово',
-                  },
-                  {
-                    value: PaymentTypeParam.EVERY_YEAR,
-                    label: 'Щорічно',
-                  },
+                  { label: 'Бюджет', value: StudyTypeParam.BUDGET },
+                  { label: 'Контракт', value: StudyTypeParam.CONTRACT },
                 ]}
-                clearValueOnUnmount
               />
             </Box>
-          )}
-          <Box sx={stylesMui.item}>
-            <Typography variant="h6Bold">
-              Форма навчання (денна/заочна)
-            </Typography>
-            <FormikRadioGroup
-              name="meta.studyForm"
-              options={[
-                { label: 'Денна', value: StudyFormParam.FULL_TIME },
-                { label: 'Заочна', value: StudyFormParam.PART_TIME },
-              ]}
-            />
-          </Box>
+            {values.meta.studyType === StudyTypeParam.CONTRACT && (
+              <Box sx={stylesMui.item}>
+                <Typography variant="h6Bold">
+                  Оплата(Щосеместрово/Щоквартально)
+                </Typography>
+                <FormikRadioGroup
+                  name="meta.paymentType"
+                  options={[
+                    {
+                      value: PaymentTypeParam.EVERY_SEMESTER,
+                      label: 'Щосеместрово',
+                    },
+                    {
+                      value: PaymentTypeParam.EVERY_YEAR,
+                      label: 'Щорічно',
+                    },
+                  ]}
+                  clearValueOnUnmount
+                />
+              </Box>
+            )}
+            <Box sx={stylesMui.item}>
+              <Typography variant="h6Bold">
+                Форма навчання (денна/заочна)
+              </Typography>
+              <FormikRadioGroup
+                name="meta.studyForm"
+                options={[
+                  { label: 'Денна', value: StudyFormParam.FULL_TIME },
+                  { label: 'Заочна', value: StudyFormParam.PART_TIME },
+                ]}
+              />
+            </Box>
 
-          <Box sx={stylesMui.item}>
-            <Typography variant="h6Bold">Спеціальність</Typography>
-            <FormikRadioGroup
-              name="meta.speciality"
-              options={[
-                {
-                  value: '121',
-                  label: '121 Інженерія програмного забезпечення',
-                },
-                { value: '123', label: '123 Комп’ютерна інженерія' },
-                {
-                  value: '126',
-                  label: '126 Інформаційні системи та технології',
-                },
-              ]}
-            />
-          </Box>
+            <Box sx={stylesMui.item}>
+              <Typography variant="h6Bold">Спеціальність</Typography>
+              <FormikRadioGroup
+                name="meta.speciality"
+                options={[
+                  {
+                    value: '121',
+                    label: '121 Інженерія програмного забезпечення',
+                  },
+                  { value: '123', label: '123 Комп’ютерна інженерія' },
+                  {
+                    value: '126',
+                    label: '126 Інформаційні системи та технології',
+                  },
+                ]}
+              />
+            </Box>
 
-          <Box sx={stylesMui.item}>
-            <CheckBox
-              name="meta.isToAdmission"
-              label="Формую договір в корпусі"
-            />
-          </Box>
-          <Box sx={stylesMui.item}>
-            <CheckBox name="helper.isAdult" label="Є 18 років" />
-          </Box>
-          <Box sx={stylesMui.item}>
-            <CheckBox
-              name="meta.isForcePushed"
-              label="Надіслати примусово (пропустіть цю опцію)"
-              onClick={handleCheck}
-            />
-          </Box>
+            <Box>
+              <Box sx={stylesMui.item}>
+                <CheckBox
+                  name="meta.isToAdmission"
+                  label="Формую договір в корпусі"
+                />
+              </Box>
+              <Box sx={stylesMui.item}>
+                <CheckBox name="helper.isAdult" label="Є 18 років" />
+              </Box>
+              <Box sx={stylesMui.item}>
+                <CheckBox
+                  name="meta.isForcePushed"
+                  label="Надіслати примусово (пропустіть цю опцію)"
+                  onClick={handleCheck}
+                />
+              </Box>
+            </Box>
 
-          <Actions isFormValid={touched && isValid} first />
+            <Actions isFormValid={touched && isValid} first />
+          </Stack>
         </Form>
       )}
     </Formik>
