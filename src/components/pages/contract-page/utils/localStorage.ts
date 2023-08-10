30cde79e4ef41895da64429fefed4c4d06b2f24d
kujo205 <kuc8301@gmail.com>
2023-08-10 17:49:39 +0300

    fixed localstorage bug

--- src/components/pages/contract-page/utils/localStorage.ts
+++ src/components/pages/contract-page/utils/localStorage.ts
@@ -1,3 +1,4 @@
+import { initialValues } from '@/components/pages/contract-page/constants';
 import type { ExtendedContractBody } from '@/lib/api/contract/types/ContractBody';
 
 export const saveLocalStorage = (data: ExtendedContractBody | null) => {
@@ -13,11 +14,44 @@ export const getLocalStorage = (): ExtendedContractBody | null => {
   const data = localStorage.getItem('contractData');
 
   if (data) {
-    const parsedData: ExtendedContractBody = JSON.parse(data);
+    const parsedData = JSON.parse(data);
     parsedData.meta.isForcePushed = !!parsedData.meta.isForcePushed;
     parsedData.helper.hasCustomer = !!parsedData.helper.hasCustomer;
-    return parsedData;
+
+    if (compareNestedKeys(parsedData, initialValues as never))
+      return parsedData as ExtendedContractBody;
   }
 
   return null;
 };
+
+function compareNestedKeys(
+  obj1: Record<string, never>,
+  obj2: Record<string, never>,
+): boolean {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (!keys2.includes(key)) {
+      return false;
+    }
+
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!compareNestedKeys(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+
+  return true;
+}
