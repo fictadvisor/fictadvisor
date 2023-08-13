22fb402f0c72f84ab6eb75943ffb2e7f84a35c4f
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-08-13 20:16:00 +0300

    documentation/popup
    * added first button to popup storybook

--- src/components/common/ui/pop-ups-mui/Popup/Popup.stories.ts
+++ src/components/common/ui/pop-ups-mui/Popup/Popup.stories.ts
@@ -1,24 +1,38 @@
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Popup from '@/components/common/ui/pop-ups-mui/Popup';
+import Button from '@/components/common/ui/button-mui/Button';
+import React from "react";
+import {ButtonColor, ButtonSize, ButtonVariant} from "@/components/common/ui/button-mui/types";
 
 const meta = {
-    title: 'Ui Kit/Components/Popup',
-    component: Popup,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
+  title: 'Ui Kit/Components/Popup',
+  component: Popup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
 } satisfies Meta<typeof Popup>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+
+
 export const Base: Story = {
-    args: {
-        text: "Some text",
-        title: "Some title",
-        open: false
-    }
+  args: {
+    text: 'Some text',
+    title: 'Some title',
+    open: false,
+    contentLeft: undefined,
+    sx: undefined,
+    hasCross: false,
+    icon: undefined,
+    firstButton: React.createElement(Button),
+    secondButton: undefined,
+    onClose: () => {},
+  },
 };
+
