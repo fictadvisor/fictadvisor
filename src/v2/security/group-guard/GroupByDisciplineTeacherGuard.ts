b06339c2e1ba741fbfe36b9a4f8345e95a6a84f2
ÐKate Srebniuk <ksreiebniukk@gmail.com>
2023-09-14 14:42:11 +0300

    enhancement #730: add check for the existence of discipline teacher
    - added an error that is generated if the teacher of such a discipline does not exist
    
    closes #730

--- src/v2/security/group-guard/GroupByDisciplineTeacherGuard.ts
+++ src/v2/security/group-guard/GroupByDisciplineTeacherGuard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { RequestUtils } from '../../utils/RequestUtils';
 import { DisciplineTeacherRepository } from 'src/v2/database/repositories/DisciplineTeacherRepository';
+import { InvalidEntityIdException } from '../../utils/exceptions/InvalidEntityIdException';
 
 @Injectable()
 export class GroupByDisciplineTeacherGuard implements CanActivate {
@@ -14,6 +15,9 @@ export class GroupByDisciplineTeacherGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<Request>();
     const disciplineTeacherId = RequestUtils.get(request, 'disciplineTeacherId');
     const teacher = await this.disciplineTeacherRepository.findById(disciplineTeacherId);
+    if (!teacher) {
+      throw new InvalidEntityIdException('Discipline teacher');
+    }
     request.query.groupId = teacher.discipline.group.id;
     return true;
   }
