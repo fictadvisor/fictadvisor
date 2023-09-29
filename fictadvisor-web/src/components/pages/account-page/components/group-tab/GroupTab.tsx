e64a5c304c5d0cee8f309451b691349c79c41011
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-09-30 02:21:54 +0300

    * add sort button to account page group tab

--- fictadvisor-web/src/components/pages/account-page/components/group-tab/GroupTab.tsx
+++ fictadvisor-web/src/components/pages/account-page/components/group-tab/GroupTab.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useQuery } from 'react-query';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
 import { Box, Typography } from '@mui/material';
 
+import Button from '@/components/common/ui/button-mui/Button';
+import {
+  IconButtonColor,
+  IconButtonShape,
+} from '@/components/common/ui/icon-button';
+import IconButton from '@/components/common/ui/icon-button-mui';
 import Progress from '@/components/common/ui/progress';
 import NoGroupBlock from '@/components/pages/account-page/components/group-tab/components/no-group-block';
 import RequestsTable from '@/components/pages/account-page/components/group-tab/components/table/requests-table';
@@ -32,6 +39,7 @@ const getStudents = async (user: User) => {
 };
 
 const GroupTab: FC = () => {
+  const [isSorted, setIsSorted] = useState(false);
   const { user } = useAuthentication();
   const { data, isLoading, refetch } = useQuery(
     ['students'],
@@ -58,13 +66,28 @@ const GroupTab: FC = () => {
     return <NoGroupBlock />;
 
   if (!data || !user?.group || !user?.group.role) return null;
+
+  const showRequests =
+    data?.requests?.length !== 0 && user?.group?.role !== UserGroupRole.STUDENT;
+  data?.requests?.length !== 0 && user?.group?.role !== UserGroupRole.STUDENT;
+
   return (
-    <>
-      <Box>
-        <Typography sx={styles.group} variant="h4">
-          Список групи {user.group.code}
-        </Typography>
+    <Box className={styles['content']}>
+      <Box className={styles['text-content']}>
+        <Typography sx={styles.group} variant="h4">Список групи {user.group.code}</Typography>
+        <IconButton
+          // TODO move inline styles when refactor
+          sx={{ height: 'fit-content', marginLeft: '10px' }}
+          shape={IconButtonShape.SQUARE}
+          color={IconButtonColor.SECONDARY}
+          icon={isSorted ? <ChevronDownIcon /> : <ChevronUpIcon />}
+          onClick={() => {
+            setIsSorted(!isSorted);
+            refetch();
+          }}
+        />
       </Box>
+
       {showRequests && (
         <RequestsTable
           refetch={refetch}
@@ -75,10 +98,18 @@ const GroupTab: FC = () => {
         <StudentsTable
           refetch={refetch}
           role={user.group.role}
-          rows={transformStudentsData(data.students)}
+          rows={
+            isSorted
+              ? transformStudentsData(
+                  data.students.sort((a, b) =>
+                    a.firstName.localeCompare(b.firstName),
+                  ),
+                )
+              : transformStudentsData(data.students)
+          }
         />
       }
-    </>
+    </Box>
   );
 };
 
