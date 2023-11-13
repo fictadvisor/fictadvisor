97d86cae086c39002883f600d71362b50539e531
Markusplay <mshilo2004@gmail.com>
2023-11-14 00:26:42 +0200

    add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import uk from 'dayjs/locale/uk';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
 import AuthenticationProvider from '@/hooks/use-authentication/authentication-context';
 import ToastContextProvider from '@/hooks/use-toast/toast-context';
@@ -28,17 +30,30 @@ if (!process.browser) React.useLayoutEffect = React.useEffect;
 
 const Application = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'uk'}>
-        <QueryClientProvider client={queryClient}>
-          <AuthenticationProvider>
-            <ToastContextProvider>
-              <Component {...pageProps} />
-            </ToastContextProvider>
-          </AuthenticationProvider>
-        </QueryClientProvider>
-      </LocalizationProvider>
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XEDY9ZSF2S"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+
+            gtag("config", "G-XEDY9ZSF2S");`}
+      </Script>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'uk'}>
+          <QueryClientProvider client={queryClient}>
+            <AuthenticationProvider>
+              <ToastContextProvider>
+                <Component {...pageProps} />
+              </ToastContextProvider>
+            </AuthenticationProvider>
+          </QueryClientProvider>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
