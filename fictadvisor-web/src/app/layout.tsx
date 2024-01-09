27134b0adba95ddc0fdc51177590af296d14573c
sifes <lonfark@gmail.com>
2024-01-09 17:41:07 +0200

    apple-touch-icon #7: add touch icon to metadata
    - add icon for apple-touch
    
    closes #7

--- fictadvisor-web/src/app/layout.tsx
+++ fictadvisor-web/src/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
         </Script>
         <script async src="https://telegram.org/js/telegram-widget.js" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
