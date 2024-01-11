b24285966c9007dccbfb7c2b8339ffe98c0f1292
Mykhailo Shylo <mshilo2004@gmail.com>
2024-01-11 18:06:41 +0200

    bug #124: fixed mobile header account tabs navigation - fixed tabs navigation - created createQueryString method to manage tabs and query mutating
    closes #124

---
+++ fictadvisor-web/src/utils/createQueryString.ts
@@ -0,0 +1,12 @@
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation';
+
+const createQueryString = (
+  name: string,
+  value: string,
+  searchParams: ReadonlyURLSearchParams,
+) => {
+  const params = new URLSearchParams(searchParams.toString());
+  params.set(name, value);
+  return '?' + params.toString();
+};
+export default createQueryString;
