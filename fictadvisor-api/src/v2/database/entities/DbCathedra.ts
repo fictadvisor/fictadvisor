f992052a9a400d10a24b9886f63b9dff66fb57d8
fujivara <marat.skyba@gmail.com>
2024-01-22 15:01:11 +0200

    migration #113: added division field to cathedras table
    - Added division text field to cathedra
    - Enriched CRUD endpoints for cathedra
    - Enriched teacher mappers
    
    closes #113

--- fictadvisor-api/src/v2/database/entities/DbCathedra.ts
+++ fictadvisor-api/src/v2/database/entities/DbCathedra.ts
@@ -4,6 +4,7 @@ export class DbCathedra {
   id: string;
   name: string;
   abbreviation: string;
+  division: string;
   teachers: (TeachersOnCathedras & { teacher: Teacher })[];
   createdAt: Date;
   updatedAt: Date;
