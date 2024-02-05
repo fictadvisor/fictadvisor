3269e8789498c8925fb81f6acbc237de1e0e7697
kujo205 <kuc8301@gmail.com>
2024-02-05 09:52:05 +0200

    fix #216: removed semester caching on schedule page

--- fictadvisor-web/src/app/schedule/page.tsx
+++ fictadvisor-web/src/app/schedule/page.tsx
@@ -1,51 +1,8 @@
-/*
-import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-
-import SchedulePage, {
-  SchedulePageProps,
-} from '@/components/pages/schedule-page/SchedulePage';
-import DatesAPI from '@/lib/api/dates/DatesAPI';
-import GroupAPI from '@/lib/api/group/GroupAPI';
-
-const SchedulePageWrapper = ({
-  groups,
-  semester,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return <SchedulePage groups={groups} semester={semester} />;
-};
-
-export const getServerSideProps: GetServerSideProps<
-  SchedulePageProps
-> = async context => {
-  try {
-    const [semestersData, groupData] = await Promise.all([
-      DatesAPI.getCurrentSemester(),
-      GroupAPI.getAll(),
-    ]);
-
-    return {
-      props: {
-        groups: groupData.groups,
-        semester: semestersData,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        groups: [],
-        semester: null,
-      },
-    };
-  }
-};
-
-export default SchedulePageWrapper;
-*/
 import SchedulePage from '@/components/pages/schedule-page/SchedulePage';
 import DatesAPI from '@/lib/api/dates/DatesAPI';
 import GroupAPI from '@/lib/api/group/GroupAPI';
 import { Group } from '@/types/group';
-
+export const dynamic = 'force-dynamic';
 export default async function SchedulePageWrapper() {
   let groups: Group[] = [];
   let semester = null;
