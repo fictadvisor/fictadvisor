9e50af524f07ccc01566a1d940d8b03a7f0ce416
lil5166 <o.semashko.ia23@kpi.ua>
2024-02-18 23:38:35 +0200

    bug #242: fix titles on home, schedule, account pages closes #242

--- fictadvisor-web/src/app/account/page.tsx
+++ fictadvisor-web/src/app/account/page.tsx
@@ -1,13 +1,11 @@
-'use client';
-
 import { Metadata } from 'next';
 
 import PageLayout from '@/components/common/layout/page-layout';
 import AccountPage from '@/components/pages/account-page';
 
-/*export const metadata: Metadata = {
+export const metadata: Metadata = {
   title: 'Персональний акаунт FICT Advisor',
-};*/
+};
 const Account = () => (
   <PageLayout>
     <AccountPage />
--- fictadvisor-web/src/app/page.tsx
+++ fictadvisor-web/src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from 'next';
+
 import MainPage from '@/components/pages/main-page';
 import { MainPageProps } from '@/components/pages/main-page/MainPage';
 import StudentResourcesAPI from '@/lib/api/student-resources/StudentResourcesAPI';
+
+export const metadata: Metadata = {
+  title: 'FICT Advisor',
+};
 
 export default async function Main() {
   let data: MainPageProps['data'];
