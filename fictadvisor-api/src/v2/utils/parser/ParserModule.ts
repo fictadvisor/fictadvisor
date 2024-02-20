82528682f580ed535bc4a055858328ac68f3814d
SharpDevOps10 <td090833@gmail.com>
2024-02-20 20:14:28 +0200

    refactoring #102: updated roz.kpi.ua parser
    closes #102

--- fictadvisor-api/src/v2/utils/parser/ParserModule.ts
+++ fictadvisor-api/src/v2/utils/parser/ParserModule.ts
@@ -3,10 +3,11 @@ import { CampusParser } from './CampusParser';
 import { RozParser } from './RozParser';
 import { PrismaModule } from '../../modules/PrismaModule';
 import { DateModule } from '../date/DateModule';
+import { GeneralParser } from './GeneralParser';
 
 @Module({
-  providers: [CampusParser, RozParser],
-  exports: [CampusParser, RozParser],
+  providers: [CampusParser, RozParser, GeneralParser],
+  exports: [CampusParser, RozParser, GeneralParser],
   imports: [PrismaModule, DateModule],
 })
 export class ParserModule {}
