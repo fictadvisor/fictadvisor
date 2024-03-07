03f4688ffa1651809ec8465a17c415eed8d9bf3c
sifes <lonfark@gmail.com>
2024-03-07 10:51:51 +0200

    bugfix #271: fixed bugs on teacher admin page
    - added edit teacher`s photo
    - added create teacher`s photo
    - fix error 404 on the admin panel teacher creation page after clicking the "Скасувати" button
    - fix error 404 on the admin panel teacher editing page after clicking the "Скасувати" button
    - fix that the field with comments is located in the center on the "Edit teacher" page
    
    closes: #271

--- fictadvisor-web/src/components/pages/admin/admin-teachers/create-teacher-admin-page/types/index.ts
+++ fictadvisor-web/src/components/pages/admin/admin-teachers/create-teacher-admin-page/types/index.ts
@@ -3,7 +3,12 @@ import { Teacher, TeacherComment } from '@/types/teacher';
 export interface PersonalInfo
   extends Pick<
     Teacher,
-    'firstName' | 'middleName' | 'lastName' | 'avatar' | 'description'
+    | 'firstName'
+    | 'middleName'
+    | 'lastName'
+    | 'avatar'
+    | 'description'
+    | 'avatar'
   > {}
 
 export interface EditedComment extends Omit<TeacherComment, 'comment'> {
