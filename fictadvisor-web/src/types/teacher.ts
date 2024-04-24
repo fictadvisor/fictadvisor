50f79f6e3bea73c7f1069a5cc71c893e8325da50
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-04-24 23:09:04 +0300

    feature #254: added new teacher card fields - Changed Teacher interface
    closes #254

--- fictadvisor-web/src/types/teacher.ts
+++ fictadvisor-web/src/types/teacher.ts
@@ -4,6 +4,8 @@ import {
   ContactResponse,
 } from '@fictadvisor/utils/responses';
 
+import { Contact } from '@/types/contact';
+
 export interface TeacherCathedra {
   id: string;
   name: string;
@@ -15,7 +17,35 @@ export interface TeacherSubject {
   name: string;
 }
 
-export class Teacher {
+export interface TeacherDiscipline {
+  disciplineTeacherId: string;
+  subjectName: string;
+}
+
+export const TeacherAcademicStatus: Record<string, string> = {
+  DOCENT: 'Доцент',
+  PROFESSOR: 'Професор',
+  SENIOR_RESEARCH_ASSISTANT: 'Старший науковий співробітник',
+};
+
+export const TeacherScientificDegree: Record<string, string> = {
+  CANDIDATE: 'Кандидат наук',
+  DOCTOR: 'Доктор наук',
+};
+
+export const TeacherPosition: Record<string, string> = {
+  ASSOCIATE_PROFESSOR: 'Доцент',
+  DEPARTMENT_CHAIR: 'Завідуючий кафедрою',
+  PROFESSOR: 'Професор',
+  RESEARCH_ASSISTANT: 'Молодший науковий співробітник',
+  SENIOR_RESEARCH_ASSISTANT: 'Старший науковий співробітник',
+  SENIOR_LECTURER: 'Старший викладач',
+  ASSISTANT: 'Асистент',
+  LEADING_RESEARCHER: 'Провідний науковий співробітник',
+  LECTURER: 'Лектор',
+};
+
+export interface Teacher {
   id: string;
   firstName: string;
   middleName: string;
@@ -24,8 +54,11 @@ export class Teacher {
   description: string;
   rating: number;
   roles: TeacherRole[];
-  contacts: ContactResponse[];
-  cathedras: CathedraResponse[];
+  contacts: Contact[];
+  cathedras: TeacherCathedra[];
+  academicStatus: string;
+  scientificDegree: string;
+  position: string;
 }
 
 export interface DisciplineTeacher extends Teacher {
