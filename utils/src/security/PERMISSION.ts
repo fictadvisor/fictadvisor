30831520bc8605af278ab65e467cbec64bae3d6d
aim4ik <rusak.yaroslav@lll.kpi.ua>
2024-04-27 19:53:16 +0300

    bugfix #376: get pagetext needs permission
    - removed permission from  get pagetext endpoint
    
    closes #376

--- utils/src/security/PERMISSION.ts
+++ utils/src/security/PERMISSION.ts
@@ -120,7 +120,6 @@ export enum PERMISSION {
 	TEACHERS_$TEACHERID_CONTACTS_UPDATE = 'teachers.$teacherId.contacts.update',
 	TEACHERS_$TEACHERID_CONTACTS_DELETE = 'teachers.$teacherId.contacts.delete',
 
-	PAGE_TEXTS_GET = 'pages.get',
 	PAGE_TEXTS_CREATE = 'pages.create',
 	PAGE_TEXTS_UPDATE = 'pages.update',
 }
