8ccf62365a9f8715179419def22724921ffaf10e
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-04-29 18:04:26 +0300

    enhancement #380: make sw for production
    - fixed sw
    - added checking on production
    
    closes #380

--- fictadvisor-web/src/app/layout.tsx
+++ fictadvisor-web/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
       navigator.serviceWorker
         .register('/service-worker.js')
         .then(registration => console.log('scope is: ', registration.scope));
