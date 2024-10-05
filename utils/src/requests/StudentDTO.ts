a876b16858a41d249463e6db9ef1ceddd0862212
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-05 13:14:01 +0300

    enhancement #513: refactored auth controller
    - added tooManyRequests property to ApiDocumentationParams
    - moved documentation to separate files
    - added missing validation decorators to some DTOs
    - removed promise return types from controller methods
    
    closes #513

--- utils/src/requests/StudentDTO.ts
+++ utils/src/requests/StudentDTO.ts
@@ -1,5 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsOptional, IsUUID, Matches } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID, Matches } from 'class-validator';
 import {
   UKR_REGEX,
   UKRSPEC_REGEX,
@@ -12,6 +12,7 @@ export class StudentDTO {
   })
   @IsUUID(undefined, validationOptionsMsg('Group id must be UUID'))
   @IsNotEmpty(validationOptionsMsg('Group id cannot be empty'))
+  @IsString(validationOptionsMsg('Group id must be string'))
     groupId: string;
 
   @ApiProperty({
@@ -21,6 +22,7 @@ export class StudentDTO {
     new RegExp('^[' + UKR_REGEX + UKRSPEC_REGEX + ']{2,40}$'),
     validationOptionsMsg('First name is not correct (A-Я(укр.)\\-\' ), or too short (min: 2), or too long (max: 40)'))
   @IsNotEmpty(validationOptionsMsg('First name cannot be empty'))
+  @IsString(validationOptionsMsg('First name must be string'))
     firstName: string;
 
   @ApiPropertyOptional({
@@ -29,6 +31,7 @@ export class StudentDTO {
   @Matches(
     new RegExp('^[' + UKR_REGEX + UKRSPEC_REGEX + ']{0,40}$'),
     validationOptionsMsg('Middle name is not correct (A-Я(укр.)\\-\' ), or too long (max: 40)'))
+  @IsString(validationOptionsMsg('Middle name must be string'))
   @IsOptional()
     middleName?: string;
 
@@ -39,6 +42,7 @@ export class StudentDTO {
     new RegExp('^[' + UKR_REGEX + UKRSPEC_REGEX + ']{2,40}$'),
     validationOptionsMsg('Last name is not correct (A-Я(укр.)\\-\' ), or too short (min: 2), or too long (max: 40)'))
   @IsNotEmpty(validationOptionsMsg('Last name cannot be empty'))
+  @IsString(validationOptionsMsg('Last name must be string'))
     lastName: string;
 
   @ApiProperty({
