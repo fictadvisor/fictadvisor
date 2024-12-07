2e046058c24553584936dc06ae0262746a9bd526
REGENS_LAPTOP\regen <regens.global@gmail.com>
2024-12-07 11:56:45 +0200

    enhancement #545: refactored date controller

---
+++ fictadvisor-api/src/v2/utils/documentation/date/index.ts
@@ -0,0 +1,8 @@
+import { DateDocumentationGetPreviousSemesters } from './getPreviousSemesters';
+import { DateDocumentationGetCurrentSemester } from './getCurrentSemester';
+
+
+export const DateDocumentation = {
+  GET_PREVIOUS_SEMESTERS: DateDocumentationGetPreviousSemesters,
+  GET_CURRENT_SEMESTER: DateDocumentationGetCurrentSemester,
+};
