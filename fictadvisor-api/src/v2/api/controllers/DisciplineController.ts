34554d90fb0265cbcc80458ec715faaffd009d5d
Vadym Chunikhin <vadimchunihin.v@gmail.com>
2024-12-09 01:52:22 +0100

    enhancement #511: Refactor Discipline entity

--- fictadvisor-api/src/v2/api/controllers/DisciplineController.ts
+++ fictadvisor-api/src/v2/api/controllers/DisciplineController.ts
@@ -1,33 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import {
-  ApiBadRequestResponse,
-  ApiCookieAuth,
-  ApiForbiddenResponse,
-  ApiOkResponse,
-  ApiParam,
-  ApiQuery,
-  ApiTags,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, ParseEnumPipe, Patch, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import {
   CreateDisciplineDTO,
   QueryAllDisciplinesDTO,
   UpdateDisciplineDTO,
 } from '@fictadvisor/utils/requests';
 import {
-  DisciplineTeachersResponse,
   ExtendedDisciplineTeachersResponse,
   DisciplinesResponse,
 } from '@fictadvisor/utils/responses';
 import { DisciplineTypeEnum } from '@fictadvisor/utils/enums';
 import { PERMISSION } from '@fictadvisor/utils/security';
 import { DisciplineService } from '../services/DisciplineService';
-import { Access } from 'src/v2/security/Access';
 import { ApiEndpoint } from '../../utils/documentation/decorators';
-import { GroupByDisciplineGuard } from '../../security/group-guard/GroupByDisciplineGuard';
 import { DisciplineByIdPipe } from '../pipes/DisciplineByIdPipe';
 import { QueryAllDisciplinesPipe } from '../pipes/QueryAllDisciplinesPipe';
 import { DisciplineMapper } from '../../mappers/DisciplineMapper';
+import { DisciplineDocumentation } from '../../utils/documentation/discipline';
+import { DisciplineTeacherMapper } from '../../mappers/DisciplineTeacherMapper';
 
 @ApiTags('Discipline')
 @Controller({
@@ -38,202 +28,87 @@ export class DisciplineController {
   constructor (
     private disciplineService: DisciplineService,
     private disciplineMapper: DisciplineMapper,
+    private disciplineTeacherMapper: DisciplineTeacherMapper,
   ) {}
 
-  @ApiCookieAuth()
-  @ApiOkResponse({
-    type: ExtendedDisciplineTeachersResponse,
-  })
-  @ApiUnauthorizedResponse({
-    description: `\n
-    UnauthorizedException:
-      Unauthorized`,
-  })
-  @ApiForbiddenResponse({
-    description: `\n
-    NoPermissionException:
-      You do not have permission to perform this action`,
-  })
   @Post()
   @ApiEndpoint({
     summary: 'Create new discipline',
+    documentation: DisciplineDocumentation.CREATE,
     permissions: PERMISSION.GROUPS_$GROUPID_DISCIPLINES_CREATE,
   })
   async create (
     @Body() body: CreateDisciplineDTO,
-  ) {
+  ): Promise<ExtendedDisciplineTeachersResponse> {
     const discipline = await this.disciplineService.create(body);
     return this.disciplineMapper.getExtendedDisciplineTeachers(discipline);
   }
 
-  @ApiOkResponse({
-    type: DisciplinesResponse,
-  })
-  @ApiBadRequestResponse({
-    description: `\n
-    InvalidBodyException: 
-      Page must be a number
-      PageSize must be a number
-      Wrong value for order
-      Wrong value for sort
-      Each value of semesters must be an studying semester
-      
-    InvalidEntityIdException:
-      Group with such id is not found
-      Teacher with such id is not found`,
-  })
   @ApiEndpoint({
     summary: 'Get all disciplines with selected filters',
+    documentation: DisciplineDocumentation.GET_ALL,
   })
   @Get()
   async getAll (
     @Query(QueryAllDisciplinesPipe) body: QueryAllDisciplinesDTO,
   ): Promise<DisciplinesResponse> {
     const disciplinesWithSelectiveAmounts = await this.disciplineService.getAll(body);
-    const disciplines = this.disciplineMapper.getDisciplinesForAdmin(disciplinesWithSelectiveAmounts.data);
+    const disciplines = this.disciplineMapper.getDisciplinesAdmin(disciplinesWithSelectiveAmounts.data);
     return {
       disciplines,
       pagination: disciplinesWithSelectiveAmounts.pagination,
     };
   }
 
-  @ApiCookieAuth()
-  @ApiOkResponse({
-    type: DisciplineTeachersResponse,
+  @ApiEndpoint({
+    summary: 'Get all teachers by discipline Id',
+    documentation: DisciplineDocumentation.GET_ALL_TEACHERS_BY_DISCIPLINE,
+    permissions: PERMISSION.GROUPS_$GROUPID_DISCIPLINES_TEACHERS_GET,
   })
-  @ApiBadRequestResponse({
-    description: `\n
-    InvalidQueryException:
-      Type of discipline must be a field of enum
-      
-    InvalidDisciplineIdException:
-      Discipline with such id is not found`,
-  })
-  @ApiUnauthorizedResponse({
-    description: `\n
-    UnauthorizedException:
-      Unauthorized`,
-  })
-  @ApiForbiddenResponse({
-    description: `\n
-    NoPermissionException:
-      You do not have permission to perform this action`,
-  })
-  @ApiParam({
-    name: 'disciplineId',
-    required: true,
-    description: 'Id of certain discipline',
-  })
-  @ApiQuery({
-    name: 'disciplineType',
-    required: true,
-    enum: DisciplineTypeEnum,
-    description: 'Discipline type of some discipline',
-  })
-  @Access(PERMISSION.GROUPS_$GROUPID_DISCIPLINES_TEACHERS_GET, GroupByDisciplineGuard)
   @Get('/:disciplineId/teachers')
-  async getAllByDiscipline (
+  async getAllTeachersByDiscipline (
     @Param('disciplineId', DisciplineByIdPipe) disciplineId: string,
-    @Query('disciplineType') disciplineType: DisciplineTypeEnum,
+    @Query('disciplineType', new ParseEnumPipe(DisciplineTypeEnum)) disciplineType: DisciplineTypeEnum,
   ) {
     const teachers = await this.disciplineService.getTeachers(disciplineId, disciplineType);
-    return { teachers };
+    return this.disciplineTeacherMapper.getDisciplineTeachersWithTeacherParams(teachers);
   }
 
-  @ApiCookieAuth()
-  @ApiOkResponse({
-    type: ExtendedDisciplineTeachersResponse,
-  })
-  @ApiBadRequestResponse({
-    description: `\n
-    InvalidEntityIdException:
-      Discipline with such id is not found`,
-  })
-  @ApiUnauthorizedResponse({
-    description: `\n
-    UnauthorizedException:
-      Unauthorized`,
-  })
-  @ApiForbiddenResponse({
-    description: `\n
-    NoPermissionException:
-      You do not have permission to perform this action`,
-  })
-  @ApiParam({
-    name: 'disciplineId',
-    required: true,
-    description: 'Id of a discipline to delete',
-  })
   @ApiEndpoint({
     summary: 'Delete discipline by id',
+    documentation: DisciplineDocumentation.DELETE_BY_ID,
     permissions: PERMISSION.DISCIPLINE_DELETE,
   })
   @Delete('/:disciplineId')
-  async delete (
+  async deleteById (
     @Param('disciplineId', DisciplineByIdPipe) disciplineId: string,
   ): Promise<ExtendedDisciplineTeachersResponse> {
-    const discipline = await this.disciplineService.deleteDiscipline(disciplineId);
+    const discipline = await this.disciplineService.deleteById(disciplineId);
     return this.disciplineMapper.getExtendedDisciplineTeachers(discipline);
   }
 
-  @ApiOkResponse({
-    type: ExtendedDisciplineTeachersResponse,
-  })
-  @ApiBadRequestResponse({
-    description: `\n
-    InvalidEntityIdException:
-      Discipline with such id is not found`,
-  })
-  @ApiParam({
-    name: 'disciplineId',
-    required: true,
-    description: 'Id of a discipline to get',
-  })
   @ApiEndpoint({
     summary: 'Get discipline by id',
+    documentation: DisciplineDocumentation.GET_BY_ID,
   })
-  @Get(':disciplineId')
-  async getById (@Param('disciplineId', DisciplineByIdPipe) disciplineId: string) {
-    const discipline = await this.disciplineService.get(disciplineId);
+  @Get('/:disciplineId')
+  async getById (
+    @Param('disciplineId', DisciplineByIdPipe) disciplineId: string
+  ): Promise<ExtendedDisciplineTeachersResponse> {
+    const discipline = await this.disciplineService.getById(disciplineId);
     return this.disciplineMapper.getExtendedDisciplineTeachers(discipline);
   }
 
-  @ApiCookieAuth()
-  @ApiOkResponse({
-    type: ExtendedDisciplineTeachersResponse,
-  })
-  @ApiBadRequestResponse({
-    description: `\n
-    InvalidEntityIdException:
-      Discipline with such id is not found
-
-    InvalidBodyException:
-      isSelective property must be boolean`,
-  })
-  @ApiUnauthorizedResponse({
-    description: `\n
-    UnauthorizedException:
-      Unauthorized`,
-  })
-  @ApiForbiddenResponse({
-    description: `\n
-    NoPermissionException: 
-      You do not have permission to perform this action`,
-  })
-  @ApiParam({
-    name: 'disciplineId',
-    required: true,
-    description: 'Id of a discipline to update',
-  })
   @ApiEndpoint({
     summary: 'Update discipline by id',
+    documentation: DisciplineDocumentation.UPDATE_BY_ID,
     permissions: PERMISSION.DISCIPLINE_UPDATE,
   })
   @Patch('/:disciplineId')
   async updateById (
     @Body() body: UpdateDisciplineDTO,
     @Param('disciplineId', DisciplineByIdPipe) disciplineId: string,
-  ) {
+  ): Promise<ExtendedDisciplineTeachersResponse> {
     const updatedDiscipline = await this.disciplineService.updateById(disciplineId, body);
     return this.disciplineMapper.getExtendedDisciplineTeachers(updatedDiscipline);
   }
