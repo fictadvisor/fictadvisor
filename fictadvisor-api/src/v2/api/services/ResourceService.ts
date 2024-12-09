d379d5ab496351ea36c12e2ae6ee962fb88b02c4
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-09 12:22:59 +0200

    enhancement #506: refactored Recource entity

--- fictadvisor-api/src/v2/api/services/ResourceService.ts
+++ fictadvisor-api/src/v2/api/services/ResourceService.ts
@@ -3,11 +3,9 @@ import {
   CreateResourceDTO,
   UpdateResourceDTO,
   UpdateResourcesDTO,
-  UpdateResourcesDTOItem,
   QueryAllResourcesDTO,
 } from '@fictadvisor/utils/requests';
 import { ResourceRepository } from '../../database/repositories/ResourceRepository';
-import { InvalidEntityIdException } from '../../utils/exceptions/InvalidEntityIdException';
 import { StudentResource } from '@prisma/client';
 
 @Injectable()
@@ -16,33 +14,31 @@ export class ResourceService {
     private resourceRepository: ResourceRepository,
   ) {}
 
-  async getAll (body: QueryAllResourcesDTO) {
+  async getAll (body: QueryAllResourcesDTO): Promise<StudentResource[]> {
     return this.resourceRepository.findMany({
       where: body.ids
         ? {
           id: {
             in: body.ids,
           },
-        } 
+        }
         : {},
     });
   }
 
-  async get (id: string) {
+  async get (id: string): Promise<StudentResource | null> {
     return this.resourceRepository.findById(id);
   }
 
-  async create (body: CreateResourceDTO) {
+  async create (body: CreateResourceDTO): Promise<StudentResource> {
     return this.resourceRepository.create(body);
   }
-    
-  async update (id: string, body: UpdateResourceDTO) {
+
+  async update (id: string, body: UpdateResourceDTO):Promise<StudentResource> {
     return this.resourceRepository.updateById(id, body);
   }
 
-  async updateMany (body: UpdateResourcesDTO) {
-    await this.resourcesValidation(body.resources);
-
+  async updateMany (body: UpdateResourcesDTO): Promise<StudentResource[]> {
     const updResources: StudentResource[] = [];
     for (const resource of body.resources) {
       const updResource = await this.update(resource.id, resource);
@@ -52,15 +48,7 @@ export class ResourceService {
     return updResources;
   }
 
-  async resourcesValidation (resources: UpdateResourcesDTOItem[]) {
-    for (const resource of resources) {
-      if (!await this.resourceRepository.find({ id: resource.id })) {
-        throw new InvalidEntityIdException('Resource');
-      }
-    }
-  }
-
-  async delete (id: string) {
+  async delete (id: string): Promise<void> {
     await this.resourceRepository.deleteById(id);
   }
 }
