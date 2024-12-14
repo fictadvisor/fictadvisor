659e7bccc95672c3ca90a76e0afa57d0efd49dfa
Гліб Подлипський <hyperlivegroup@gmail.com>
2024-12-14 01:38:34 +0000

    migration #488: remove addmision from the project
    closes #488

--- fictadvisor-web/src/app/(admission)/(main)/_entrant-dashboard/components/entrant-approve-form/validation.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const validationSchema = yup.object().shape({
-  number: yup.string().required(`Обов'язкове поле`),
-  date: yup.string().required(`Обов'язкове поле`),
-});
