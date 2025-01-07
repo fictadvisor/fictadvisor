bfb09cde4751a790efaf5e4337620dae1f346297
Veronika Maslo <144779324+nikama-s@users.noreply.github.com>
2025-01-07 14:31:22 +0200

    Corrected the fonts, changed language in the calendar, fixed padding on the poll page (#592)

--- fictadvisor-web/src/app/(main)/schedule/schedule-page/calendar-section/components/date-picker/DatePicker.tsx
+++ fictadvisor-web/src/app/(main)/schedule/schedule-page/calendar-section/components/date-picker/DatePicker.tsx
@@ -1,7 +1,11 @@
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import moment from 'moment';
 
 import { useSchedule } from '@/store/schedule/useSchedule';
+
+import 'moment/locale/uk';
 
 import * as styles from './DatePicker.styles';
 
@@ -14,22 +18,31 @@ export const DatePicker = () => {
 
   if (!chosenDay) return <></>;
 
+  moment.locale('uk', {
+    months: moment
+      .localeData('uk')
+      .months()
+      .map(month => month.charAt(0).toUpperCase() + month.slice(1)),
+  });
+
   return (
-    <DateCalendar
-      value={moment(chosenDay)}
-      onChange={newValue => {
-        if (newValue && semester) {
-          setChosenDay(newValue);
-        }
-      }}
-      sx={styles.picker}
-      views={['day']}
-      showDaysOutsideCurrentMonth
-      dayOfWeekFormatter={day => {
-        return day.charAt(0).toUpperCase() + day.slice(1);
-      }}
-      minDate={moment(semester?.startDate)}
-      maxDate={moment(semester?.endDate)}
-    />
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <DateCalendar
+        value={moment(chosenDay)}
+        onChange={newValue => {
+          if (newValue && semester) {
+            setChosenDay(newValue);
+          }
+        }}
+        sx={styles.picker}
+        views={['day']}
+        showDaysOutsideCurrentMonth
+        dayOfWeekFormatter={day => {
+          return day.charAt(0).toUpperCase() + day.slice(1);
+        }}
+        minDate={moment(semester?.startDate)}
+        maxDate={moment(semester?.endDate)}
+      />
+    </LocalizationProvider>
   );
 };
